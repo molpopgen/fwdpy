2fd630d286fb2e7b834059249b04a1041c7156d5
Kevin R. Thornton <krthornt@uci.edu>
2015-08-04 10:52:42 -0700

    update types

--- types.hpp
+++ types.hpp
@@ -5,7 +5,25 @@
 #include <fwdpp/sugar/GSLrng_t.hpp>
 namespace fwdpy {
   using GSLrng_t = KTfwd::GSLrng_t<KTfwd::GSL_RNG_MT19937>;
-  using singlepop_t = KTfwd::singlepop_serialized<KTfwd::popgenmut,KTfwd::mutation_writer,KTfwd::mutation_reader<KTfwd::popgenmut>>;
+
+  struct singlepop_t : public KTfwd::singlepop_serialized<KTfwd::popgenmut,KTfwd::mutation_writer,KTfwd::mutation_reader<KTfwd::popgenmut>>
+  {
+    using base = KTfwd::singlepop_serialized<KTfwd::popgenmut,KTfwd::mutation_writer,KTfwd::mutation_reader<KTfwd::popgenmut>>;
+    unsigned generation;
+    singlepop_t(const unsigned & N) : base(N),generation(0)
+      {
+      }
+  };
+
+  struct metapop_t : public KTfwd::metapop_serialized<KTfwd::popgenmut,KTfwd::mutation_writer,KTfwd::mutation_reader<KTfwd::popgenmut> >
+  {
+    using base = KTfwd::metapop_serialized<KTfwd::popgenmut,KTfwd::mutation_writer,KTfwd::mutation_reader<KTfwd::popgenmut> >;
+    unsigned generation;
+    metapop_t( std::initializer_list<unsigned> Ns ) : base(Ns), generation(0)
+    {
+    }
+  };
+  
 }
 
 #endif
