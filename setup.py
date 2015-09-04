084a648c6d52d380879a70672b03dfa089a5a7b0
Kevin R. Thornton <krthornt@uci.edu>
2015-09-04 08:08:22 -0700

    more stuff in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+from setuptools import install_requires
 from Cython.Build import cythonize
 
-setup(author='Kevin R. Thornton',
+setup(name="fwdpy",
+        version="0.0.1",
+        author='Kevin R. Thornton',
         author_email='krthornt@uci.edu',
+        license = "GSL",
+        url = "http://github.com/molpopgen/fwdpy",
         ext_modules = cythonize(Extension(
-        "fwdpy",                                # the extesion name
-        sources=["fwdpy.pyx"], # the Cython source and additional C++ source files
-        language="c++",                        # generate and compile C++ code
-        extra_compile_args=["-std=c++11","-I.","-I.."],
-        extra_link_args=["-std=c++11"],
-        libraries=["gsl","gslcblas","tcmalloc","sequence"]
-      )))
+            "fwdpy",                                # the extesion name
+            sources=["fwdpy.pyx"], # the Cython source and additional C++ source files
+            language="c++",                        # generate and compile C++ code
+            extra_compile_args=["-std=c++11","-I.","-I.."],
+            extra_link_args=["-std=c++11"],
+            libraries=["gsl","gslcblas","tcmalloc","sequence"]))
+      )
