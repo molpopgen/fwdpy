94d1664f7785eabdded391cfee6ba95de0bf0c78
Kevin R. Thornton <krthornt@uci.edu>
2015-09-04 07:56:55 -0700

    author info

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize(Extension(
-           "fwdpy",                                # the extesion name
-           sources=["fwdpy.pyx"], # the Cython source and
-                                                  # additional C++ source files
-           language="c++",                        # generate and compile C++ code
-           extra_compile_args=["-std=c++11","-I.","-I.."],
-           extra_link_args=["-std=c++11"],
-           libraries=["gsl","gslcblas","tcmalloc","sequence"]
+setup(author='Kevin R. Thornton',
+        author_email='krthornt@uci.edu',
+        ext_modules = cythonize(Extension(
+        "fwdpy",                                # the extesion name
+        sources=["fwdpy.pyx"], # the Cython source and additional C++ source files
+        language="c++",                        # generate and compile C++ code
+        extra_compile_args=["-std=c++11","-I.","-I.."],
+        extra_link_args=["-std=c++11"],
+        libraries=["gsl","gslcblas","tcmalloc","sequence"]
       )))
