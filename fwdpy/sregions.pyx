0d316c6f7f1dbb2857964354541468e22f70f4b5
Kevin R. Thornton <krthornt@uci.edu>
2015-09-08 13:10:42 -0700

    allow for coupling of weigth and length

--- fwdpy/sregions.pyx
+++ fwdpy/sregions.pyx
@@ -1,41 +1,42 @@
 ## Testing
 class Region(object):
-    def __init__(self,beg,end,weight):
+    def __init__(self,beg,end,weight,coupled):
         self.b=beg
         self.e=end
         self.w=weight
+        self.c=coupled
 
 class Sregion(Region):
-    def __init__(self,beg,end,weight,h=1):
+    def __init__(self,beg,end,weight,h=1.0,coupled=False):
         self.h=h
-        super(Sregion,self).__init__(beg,end,weight)
+        super(Sregion,self).__init__(beg,end,weight,coupled)
         
 class GammaS(Sregion):
-    def __init__(self,beg,end,weight,mean,shape,h=1):
+    def __init__(self,beg,end,weight,mean,shape,h=1.0,coupled=False):
         self.mean=mean
         self.shape=shape
-        super(GammaS,self).__init__(beg,end,weight,h)
+        super(GammaS,self).__init__(beg,end,weight,h,coupled)
 
 class ConstantS(Sregion):
-    def __init__(self,beg,end,weight,s,h=1):
+    def __init__(self,beg,end,weight,s,h=1.0,coupled=False):
         self.s=s
-        super(ConstantS,self).__init__(beg,end,weight,h)
+        super(ConstantS,self).__init__(beg,end,weight,h,coupled)
 
 class UniformS(Sregion):
-    def __init__(self,beg,end,weight,lo,hi,h=1):
+    def __init__(self,beg,end,weight,lo,hi,h=1.0,coupled=False):
         self.lo=lo
         self.hi=hi
-        super(ConstantS,self).__init__(beg,end,weight,h)
+        super(ConstantS,self).__init__(beg,end,weight,h,coupled)
 
 class ExpS(Sregion):
-    def __init__(self,beg,end,weight,mean,h=1):
+    def __init__(self,beg,end,weight,mean,h=1.0,coupled=False):
         self.mean=mean
-        super(ExpS,self).__init__(beg,end,weight,h)
+        super(ExpS,self).__init__(beg,end,weight,h,coupled)
 
 class GaussianS(Sregion):
-    def __init__(self,beg,end,weight,sd,h=1):
+    def __init__(self,beg,end,weight,sd,h=1.0,coupled=False):
         self.sd=sd
-        super(GaussianS,self).__init__(beg,end,weight,h)
+        super(GaussianS,self).__init__(beg,end,weight,h,coupled)
 
 def process_regions(list l):
     starts=list()
@@ -45,7 +46,10 @@ def process_regions(list l):
         if isinstance(l[i],Region):
             starts.append(l[i].b)
             stops.append(l[i].e)
-            weights.append(l[i].w)
+            wTemp=l[i].w
+            if(l[i].c):
+                wTemp = wTemp*(l[i].e-l[i].b)
+            weights.append(wTemp)
         else:
             raise ValueError("invalid callback type encountered")
     return pandas.DataFrame({'beg':starts,'end':stops,'weight':weights})
