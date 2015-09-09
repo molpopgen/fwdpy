48c266c00378e1f5d759965a7efb91dc6ef41071
Kevin R. Thornton <krthornt@uci.edu>
2015-09-08 17:58:31 -0700

    works!  now need to make sure code is reusable

--- fwdpy/src/evolve_regions.cc
+++ fwdpy/src/evolve_regions.cc
@@ -79,7 +79,17 @@ namespace fwdpy {
 			 const std::vector<double> & rweight,
 			 const char * fitness)
   {
-    std::cerr << "A " << nbegs.size() << ' ' << nends.size() << '\n';
+    std::cerr << "A " << nbegs.size() << ' ' << nends.size() << ' ' << callbacks->size() << '\n';
+    for( unsigned i = 0 ; i < nbegs.size() ; ++i)
+      std::cerr <<"n: "<< nbegs[i] << ' ' << nends[i] <<' ' << nweights[i] << '\n';
+
+    for(unsigned i = 0 ; i < sbegs.size() ; ++i )
+      std::cerr << "s: "<< sbegs[i] << ' ' << sends[i] << ' ' << sweights[i] << '\n';
+
+    for(unsigned i=0;i<callbacks->size() ;++i)
+      std::cerr << "c: "<< typeid(callbacks->operator[](i).s).name() << ' '
+		<< typeid(callbacks->operator[](i).h).name() << '\n';
+    
     const KTfwd::extensions::discrete_mut_model m(nbegs,nends,nweights,sbegs,sends,sweights,*callbacks);
     std::cerr << "B\n";
     auto recmap = KTfwd::extensions::discrete_rec_model(rbeg,rend,rweight);
@@ -96,6 +106,7 @@ namespace fwdpy {
 	threads[i]=std::thread(evolve_regions_details,pops->operator[](i).get(),rngs[i].get(),popsizes,
 			       mu_neutral,mu_selected,littler,f,fitness,std::cref(m),std::cref(recmap));
       }
+    for(unsigned i=0;i<threads.size();++i) threads[i].join();
   }
 }
 
