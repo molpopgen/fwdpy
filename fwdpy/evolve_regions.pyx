351095a7980587a645d45cf9385b676986be1d9c
Kevin R. Thornton <krthornt@uci.edu>
2015-09-09 08:28:45 -0700

    cleanup and .tolist() fun

--- fwdpy/evolve_regions.pyx
+++ fwdpy/evolve_regions.pyx
@@ -44,6 +44,6 @@ def evolve_regions_more(GSLrng rng,
     recreg = process_regions(recregions)
     v = shwrappervec()
     process_sregion_callbacks(v,sregions)
-    evolve_regions_t(rng.thisptr,&pops.pops,&nlist[0],len(nlist),mu_neutral,mu_selected,recrate,f,nreg['beg'],nreg['end'],nreg['weight'],
-                    sreg[0]['beg'],sreg[0]['end'],sreg[0]['weight'],&v.vec,recreg['beg'],recreg['end'],recreg['weight'],
+    evolve_regions_t(rng.thisptr,&pops.pops,&nlist[0],len(nlist),mu_neutral,mu_selected,recrate,f,nreg['beg'].tolist(),nreg['end'].tolist(),nreg['weight'].tolist(),
+                    sreg['beg'].tolist(),sreg['end'].tolist(),sreg['weight'].tolist(),&v.vec,recreg['beg'].tolist(),recreg['end'].tolist(),recreg['weight'].tolist(),
                     fitness)
