ae151d8d912ba658b0c829b1c8682422e41eccb6
Kevin R. Thornton <krthornt@uci.edu>
2015-09-11 16:55:05 -0700

    overload of get_sh

--- include/sample.hpp
+++ include/sample.hpp
@@ -20,6 +20,12 @@ namespace fwdpy {
 	       std::vector<double> * h,
 	       std::vector<double> * p,
 	       std::vector<double> * a);
+  void get_sh( const std::vector<std::pair<double,std::string> > & samples,
+	       const metapop_t * pop,
+	       std::vector<double> * s,
+	       std::vector<double> * h,
+	       std::vector<double> * p,
+	       std::vector<double> * a);
   double tajd( const std::vector<std::pair<double,std::string> > & __data );
 }
 
