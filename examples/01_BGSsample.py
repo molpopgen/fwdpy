d5e04e0d0dc846a218555148d54e085844522282
Kevin R. Thornton <krthornt@uci.edu>
2015-09-15 16:08:37 -0700

    adding example files

--- examples/01_BGSsample.py
+++
@@ -1,8 +0,0 @@
-##Sample from the populations simulated in 00_BGS.py
-
-##Run the other script
-import example_00_BGS
-
-#Use a list comprehension to get a random sample of size
-#n = 20 from each replicate
-samples = [fp.get_samples(rng,i,20) for i in pops]
