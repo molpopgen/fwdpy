254df97e1dc60975b2815d3c5c324a1f239cc3c9
Kevin R. Thornton <krthornt@uci.edu>
2015-09-22 14:01:08 -0700

    update to libseq 1.8.7 sliding windows API

--- fwdpy/fwdpy/sample.cc
+++ fwdpy/fwdpy/sample.cc
@@ -226,13 +226,14 @@ namespace fwdpy {
   sliding_windows_cpp( const std::vector<std::pair<double,std::string> > & sample,
 		       const double window_size,
 		       const double steplen,
-		       const double starting_pos )
+		       const double starting_pos,
+		       const double ending_pos)
   {
     std::vector< std::vector<std::pair<double,std::string> > > rv;
     if(sample.empty()) return rv;
     
     Sequence::SimData d(sample.begin(),sample.end());
-    Sequence::PolyTableSlice<Sequence::SimData> s(d.sbegin(),d.send(),window_size,steplen,starting_pos);
+    Sequence::PolyTableSlice<Sequence::SimData> s(d.sbegin(),d.send(),window_size,steplen,starting_pos,ending_pos);
     std::for_each(s.cbegin(),s.cend(),[&rv](const Sequence::PolyTableSlice<Sequence::SimData>::const_iterator::value_type & v) {
 	rv.push_back( std::vector<std::pair<double,std::string> >(v.first,v.second) );
       });
