beb4628400dc5af1dd41b39b73a041cb723c73b4
Kevin R. Thornton <krthornt@uci.edu>
2015-09-24 14:30:10 -0700

    mutation model implemented

--- fwdpy/qtrait/ewbw.cc
+++ fwdpy/qtrait/ewbw.cc
@@ -16,12 +16,60 @@
   1. Keep a totally different data type relating mutation to trait size
 
   Both have pros and cons...
+
+  I think I like the following:
+
+  vector<pair<double,double> >
+
+  we'll keep it sorted and use log-time search
 */
+
+using namespace std;
+using esize_lookup = vector<pair<double,double> >;
 
 namespace fwdpy
 {
   namespace qtrait
   {
+    inline KTfwd::popgenmut ewbw_mut_model(gsl_rng * r,
+					   singlepop_t::lookup_table_t * lookup,
+					   esize_lookup * esizes,
+					   const unsigned & generation,
+					   const double & neutral,
+					   const double & selected,
+					   const double & h,
+					   const double & tau,
+					   const double & sigma)
+    {
+      double pos = gsl_rng_uniform(r);
+      while(lookup->find(pos)!=lookup->end())
+	{
+	  pos = gsl_rng_uniform(r);
+	}
+      lookup->insert(pos);
+
+      if( gsl_rng_uniform(r) < neutral/(neutral+selected) )
+	{
+	  //no effect on trait value
+	  return KTfwd::popgenmut(pos,0.,0.,generation,1);
+	}
+      //mutation affects trait value and has selection coefficient
+      //effect size for mutation
+      double z = gsl_ran_gaussian_ziggurat(r,1.);
+      //update esizes
+      auto itr = std::lower_bound(esizes->begin(),esizes->end(),pos,
+				  [](const esize_lookup::value_type & p,
+				     const double & __val)
+				  {
+				    return p.first < __val;
+				  });
+      esizes->emplace(itr,make_pair(pos,z));
+      double epsilon = gsl_ran_gaussian_ziggurat(r,sigma);
+      double temp = fabs(z/(1.+epsilon));
+      // s = exp(log(temp)/tau)
+      return KTfwd::popgenmut(pos,exp(log(temp)/tau),h,generation,1);
+    }
+    
     void qtrait_sim_details_t( gsl_rng * rng,
 			       fwdpy::singlepop_t * pop,
 			       const unsigned * Nvector,
@@ -29,15 +77,19 @@ namespace fwdpy
 			       const double & neutral,
 			       const double & selected,
 			       const double & recrate,
+			       const double & tau,
+			       const double & sigma,
+			       const double & h,
 			       const double & f,
 			       const bool track,  //do we want to track the trajectories of all mutations?
-			       const ew_backwards_rules & model_rules)   
+			       const ew_backwards_rules & model_rules,
+			       esize_lookup * esizes) //this is our "hack"
   {
     const unsigned simlen = Nvector_len;
     
     const double mu_tot = neutral + selected;
     
-    std::function<double(void)> recmap = std::bind(gsl_rng_uniform,rng);
+    function<double(void)> recmap = bind(gsl_rng_uniform,rng);
     for( unsigned g = 0 ; g < simlen ; ++g, ++pop->generation )
       {
 	const unsigned nextN = 	*(Nvector+g);
@@ -48,20 +100,18 @@ namespace fwdpy
 					    pop->N,
 					    nextN,
 					    mu_tot,
-					    //NEED TO FIX MUTATION MODEL -- this is copied from diploid_ind.cc as placeholder
-					    std::bind(KTfwd::infsites(),rng,&pop->mut_lookup,pop->generation,
-						      neutral,0.,[&rng](){return gsl_rng_uniform(rng);},[](){return 0.;},[](){return 0.;}),
+					    bind(ewbw_mut_model,rng,&pop->mut_lookup,esizes,pop->generation,neutral,selected,h,tau,sigma),
 					    //The recombination policy includes the uniform crossover rate
-					    std::bind(KTfwd::genetics101(),std::placeholders::_1,std::placeholders::_2,
-						      std::placeholders::_3,
+					    bind(KTfwd::genetics101(),placeholders::_1,placeholders::_2,
+						      placeholders::_3,
 						      //Pass as reference
-						      std::ref(pop->neutral),std::ref(pop->selected),
+						      ref(pop->neutral),ref(pop->selected),
 						      &pop->gametes,
 						      recrate,
 						      rng,
 						      recmap),
-					    std::bind(KTfwd::insert_at_end<typename fwdpy::singlepop_t::mutation_t,typename fwdpy::singlepop_t::mlist_t>,std::placeholders::_1,std::placeholders::_2),
-					    std::bind(KTfwd::insert_at_end<typename fwdpy::singlepop_t::gamete_t,typename fwdpy::singlepop_t::glist_t>,std::placeholders::_1,std::placeholders::_2),
+					    bind(KTfwd::insert_at_end<typename fwdpy::singlepop_t::mutation_t,typename fwdpy::singlepop_t::mlist_t>,placeholders::_1,placeholders::_2),
+					    bind(KTfwd::insert_at_end<typename fwdpy::singlepop_t::gamete_t,typename fwdpy::singlepop_t::glist_t>,placeholders::_1,placeholders::_2),
 					    //We use an empty fitness fxn here b/c the rules policies keep track of it separately.
 					    []( typename fwdpy::singlepop_t::dipvector_t::const_iterator & ) { return 0.; },
 					    KTfwd::remove_nothing(),
