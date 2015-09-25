b95905d1608474bcf7117c71d91effce835ce011
Kevin R. Thornton <krthornt@uci.edu>
2015-09-25 13:56:21 -0700

    cython code for ewvw model implemented

--- fwdpy/qtrait/ew2010.pyx
+++ fwdpy/qtrait/ew2010.pyx
@@ -1,3 +1,47 @@
+def evolve_ewvw(GSLrng rng,
+                int npops,
+                int N,
+                unsigned[:] nlist,
+                double mu_neutral,
+                double mu_selected,
+                double recrate,
+                list nregions,
+                list sregions,
+                list recregions,
+                double prop_vw,
+                double optimum = 0.,
+                bint track = False,
+                double f = 0.):
+    """
+    Eyre-Walker's 2010 model, modified so that the trait contributes to a portion of total variance in fitness.
+
+    :param rng: a :class:`GSLrng`
+    :param npops: The number of populations to simulate.  This is equal to the number of threads that will be used!
+    :param N: The diploid population size to simulate
+    :param nlist: An array view of a NumPy array.  This represents the population sizes over time.  The length of this view is the length of the simulation in generations. The view must be of an array of 32 bit, unsigned integers (see example).
+    :param mu_neutral: The mutation rate to variants not affecting fitness ("neutral" mutations).  The unit is per gamete, per generation.
+    :param mu_selected: The mutation rate to variants affecting fitness ("selected" mutations).  The unit is per gamete, per generation.
+    :param recrate: The recombination rate in the regions (per diploid, per generation)
+    :param nregions: A list specifying where neutral mutations occur
+    :param sregions: A list specifying where selected mutations occur
+    :param recregions: A list specifying how the genetic map varies along the region
+    :param prop_vw: The proportion of total variance in fitness due to this trait
+    :param optimum: The optimum trait value.
+    :param track: whether or not to record the frequency trajectories of mutations.  True = simulations are much slower!
+    :param f: The selfing probabilty
+    """
+    pops = popvec(npops,N)
+    nreg = internal.process_regions(nregions)
+    sreg = internal.process_regions(sregions)
+    recreg = internal.process_regions(recregions)
+    v = shwrappervec()
+    internal.process_sregion_callbacks(v,sregions)
+    evolve_ewvw_t(rng.thisptr,&pops.pops,&nlist[0],len(nlist),mu_neutral,mu_selected,recrate,f,prop_vw,optimum,track,
+                    nreg['beg'].tolist(),nreg['end'].tolist(),nreg['weight'].tolist(),
+                    sreg['beg'].tolist(),sreg['end'].tolist(),sreg['weight'].tolist(),&v.vec,
+                    recreg['beg'].tolist(),recreg['end'].tolist(),recreg['weight'].tolist())
+    return pops
+
 def ew2010_effects(GSLrng rng, singlepop pop, double tau, double sigma):
     """
     :param pop: A :class:`fwdpy.fwdpy.singlepop` simulated using :func:`fwdpy.qtrait.qtrait.evolve_qtrait` and/or :func:`fwdp.qtrait.qtrait.evolve_qtrait_more`
--- fwdpy/qtrait/qtrait.pyx
+++ fwdpy/qtrait/qtrait.pyx
@@ -39,8 +39,29 @@ cdef extern from "qtraits.hpp" namespace "fwdpy::qtrait":
        double p
     map[double,ew_mut_details] ew2010_assign_effects(GSLrng_t * rng, const singlepop_t * pop, const double tau, const double sigma) except +
     vector[double] ew2010_traits_cpp(const singlepop_t * pop, const map[double,ew_mut_details] & effects) except +
-    
 
+cdef extern from "ewvw.hpp" namespace "fwdpy::qtrait":
+    void evolve_ewvw_t( GSLrng_t * rng, vector[shared_ptr[singlepop_t]] * pops,
+        const unsigned * Nvector,
+        const size_t Nvector_length,
+        const double mu_neutral,
+        const double mu_selected,
+        const double littler,
+        const double f,
+        const double prop_vw,
+        const double optimum,
+        const int track,
+        const vector[double] & nbegs,
+        const vector[double] & nends,
+        const vector[double] & nweights,
+        const vector[double] & sbegs,
+        const vector[double] & sends,
+        const vector[double] & sweights,
+        const vector[shmodel] * callbacks,
+        const vector[double] & rbeg,
+        const vector[double] & rend,
+        const vector[double] & rweight)
+    
 include "evolve_qtraits.pyx"
 include "ew2010.pyx"
 include "misc.pyx"
