321b8b5432c08d5ad7dae76cf2b725b910478c4e
Kevin R. Thornton <krthornt@uci.edu>
2015-09-28 14:19:52 -0700

    update name

--- fwdpy/internal/internal.pyx
+++ fwdpy/internal/internal.pyx
@@ -6,9 +6,9 @@ include "slim.pyx"
 
 cdef class region_manager_wrapper:
     def __cinit__(self):
-        self.rm = new region_manager()
+        self.thisptr = new region_manager()
     def __dealloc__(self):
-        del self.rm
+        del self.thisptr
         
 ##Returns a region_manager_wrapper
 def make_region_manager(list nregions,
@@ -20,14 +20,14 @@ def make_region_manager(list nregions,
     v = shwrappervec()
     process_sregion_callbacks(v,sregions)
     rv = region_manager_wrapper()
-    rv.rm.callbacks = v.vec
-    rv.rm.nb = nreg['beg'].tolist()
-    rv.rm.ne = nreg['end'].tolist()
-    rv.rm.nw = nreg['weight'].tolist()
-    rv.rm.sb = sreg['beg'].tolist()
-    rv.rm.se = sreg['end'].tolist()
-    rv.rm.sw = sreg['weight'].tolist()
-    rv.rm.rb = sreg['beg'].tolist()
-    rv.rm.re = sreg['end'].tolist()
-    rv.rm.rw = sreg['weight'].tolist()
+    rv.thisptr.callbacks = v.vec
+    rv.thisptr.nb = nreg['beg'].tolist()
+    rv.thisptr.ne = nreg['end'].tolist()
+    rv.thisptr.nw = nreg['weight'].tolist()
+    rv.thisptr.sb = sreg['beg'].tolist()
+    rv.thisptr.se = sreg['end'].tolist()
+    rv.thisptr.sw = sreg['weight'].tolist()
+    rv.thisptr.rb = sreg['beg'].tolist()
+    rv.thisptr.re = sreg['end'].tolist()
+    rv.thisptr.rw = sreg['weight'].tolist()
     return rv;
