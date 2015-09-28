fe36bb65261cd2825fe18baad24a0942dd04a78c
Kevin R. Thornton <krthornt@uci.edu>
2015-09-28 13:53:33 -0700

    added constructor

--- include/internal.hpp
+++ include/internal.hpp
@@ -13,6 +13,18 @@ namespace fwdpy
     {
       std::vector<KTfwd::extensions::shmodel> callbacks;
       std::vector<double> nb,ne,nw,sb,se,sw,rb,re,rw;
+      region_manager() : callbacks(std::vector<KTfwd::extensions::shmodel>()),
+			 nb(std::vector<double>()),
+			 ne(std::vector<double>()),
+			 nw(std::vector<double>()),
+			 sb(std::vector<double>()),
+			 se(std::vector<double>()),
+			 sw(std::vector<double>()),
+			 rb(std::vector<double>()),
+			 re(std::vector<double>()),
+			 rw(std::vector<double>())
+      {
+      }
     };
   }
 }
