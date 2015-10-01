b7ea66f6245a191ac4e379e08faafbde6bb62252
Kevin R. Thornton <krthornt@uci.edu>
2015-10-01 14:23:31 -0700

    docstring, and deal with exceptions

--- fwdpy/libseq/libseq.pxd
+++ fwdpy/libseq/libseq.pxd
@@ -5,6 +5,6 @@ from libcpp.map cimport map
 
 cdef extern from "libseq.hpp" namespace "fwdpy::libseq":
     map[string,double] libseq_basic_stats( const vector[pair[double,string]] & __data )
-    map[string,double] libseq_extra_ld_stats( const vector[pair[double,string]] & __data, const double minfreq, const double binsize)
-    map[string,double] libseq_extra_ld_stats( const vector[pair[double,string]] & __data, const double minfreq, const double binsize, const vector[double] & gmap)
+    map[string,double] libseq_extra_ld_stats( const vector[pair[double,string]] & __data, const double minfreq, const double binsize) except +
+    map[string,double] libseq_extra_ld_stats( const vector[pair[double,string]] & __data, const double minfreq, const double binsize, const vector[double] & gmap) except +
     vector[vector[pair[double,string]]] sliding_windows_cpp( const vector[pair[double,string]] & sample, const double window_size, const double steplen, const double starting_pos, const double ending_pos ) except +
--- fwdpy/libseq/summstats.pyx
+++ fwdpy/libseq/summstats.pyx
@@ -3,8 +3,6 @@ def summstats(vector[pair[double,string]] data ):
     Calculate common summary statistics from a sample
 
     :param data: a sample from a population.  For example, the return value of :func:`fwdpy.fwdpy.ms_sample` or :func:`fwdpy.fwdpy.get_samples`
-
-    :raises: RuntimeError if data are not sorted in increasing order according to position
     
     Example:
     
@@ -28,6 +26,17 @@ def hapstats(vector[pair[double,string]] data,
     :param minfreq: exclude sites with minor allele frequency < minfreq
     :param binsize: The frequency window size for binning
     :param gmap: A list of values containing the position of each variant in the data's location on the genetic map.
+
+    :raises: RuntimeError if data are not sorted in increasing order according to position
+    
+    Example:
+    
+    >>> import fwdpy
+    >>> import fwdpy.libseq as lseq
+    >>> rng = fwdpy.GSLrng(100)
+    >>> pop = fwdpy.evolve_pops_t(rng,3,1000,[1000]*1000,50,50)
+    >>> s = [fwdpy.ms_sample(rng,i,10) for i in pop]
+    >>> d = [lseq.hapstats(si,0.05,0.05) for si in s]
     """
     if gmap is None:
         return libseq_extra_ld_stats(data,minfreq,binsize)
