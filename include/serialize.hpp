d68d32edd3867b6277f2560e379897a093af69a5
Kevin R. Thornton <krthornt@uci.edu>
2015-10-06 11:41:16 -0700

    added serialization for metapops

--- include/serialize.hpp
+++ include/serialize.hpp
@@ -13,6 +13,8 @@ namespace fwdpy
   {
     std::string serialize_singlepop(const singlepop_t * spop);
     std::vector<std::shared_ptr<singlepop_t> > deserialize_singlepop(const std::vector<std::string> & strings);
+    std::string serialize_metapop(const fwdpy::metapop_t * pop);
+    std::vector<std::shared_ptr<metapop_t> > deserialize_metapop(const std::vector<std::string> & strings);
   }
 }
   
