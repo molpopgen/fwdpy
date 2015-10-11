fa27ff3fa56a050e7a9188b845c40748747990af
Kevin R. Thornton <krthornt@uci.edu>
2015-10-11 11:25:09 -0700

    cleanup

--- include/sample.hpp
+++ include/sample.hpp
@@ -4,17 +4,7 @@
 #include <types.hpp>
 
 namespace fwdpy {
-  // std::vector<std::pair<double,std::string> > take_sample_from_pop(GSLrng_t * rng,const singlepop_t * pop,const unsigned nsam,const int remove_fixed);
-  // std::pair<std::vector<std::pair<double,std::string> >,
-  // 			std::vector<std::pair<double,std::string> > >
-  // take_sample_from_pop_sep(GSLrng_t * rng,const singlepop_t * pop,const unsigned nsam, const int remove_fixed);
-  // std::pair<std::vector<std::pair<double,std::string> >,
-  // 	    std::vector<std::pair<double,std::string> > >
-  // take_sample_from_metapop_sep(GSLrng_t * rng,const metapop_t * mpop,const unsigned & nsam, const int remove_fixed, const int deme);
-  //std::pair< std::vector<std::pair<double,std::string> >,
-  // 	     std::vector<std::pair<double,std::string> > >
-  //sample_specific_diploids(const singlepop_t * pop, const std::vector<unsigned> & indlist, const int remove_fixed);
-  void get_sh( const std::vector<std::pair<double,std::string> > & ms_sample,
+    void get_sh( const std::vector<std::pair<double,std::string> > & ms_sample,
 	       const singlepop_t * pop,
 	       std::vector<double> * s,
 	       std::vector<double> * h,
@@ -26,16 +16,6 @@ namespace fwdpy {
 	       std::vector<double> * h,
 	       std::vector<double> * p,
 	       std::vector<double> * a);
-  /*
-  std::map<std::string, std::vector<double> > diploid_view_cpp(const singlepop_t *pop,
-							       const size_t ind,
-							       const int remove_fixed);
-
-  std::map<std::string, std::vector<double> > diploid_view_cpp(const metapop_t * pop,
-							       const size_t ind,
-							       const int remove_fixed,
-							       const int deme);
-  */
 }
 
 #endif
