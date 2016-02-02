142c03040d675fc8479324168963e54543c23543
Kevin R. Thornton <krthornt@uci.edu>
2016-02-02 07:15:48 -0800

    heavy-duty test

--- test_qtrait.py
+++ test_qtrait.py
@@ -3,16 +3,16 @@ import fwdpy.qtrait as qt
 import numpy as np
 import pandas
 rng = fp.GSLrng(101)
-nlist = np.array([1000]*10000,dtype=np.uint32)
+
 
 hdf = pandas.HDFStore("foo2.h5",'w')
 hdf.open()
 k=0
-for i in range(2):
-    print i
+for i in range(250):
+    nlist = np.array([1000]*9000,dtype=np.uint32)
     #Evolve to equilibrium
     pops = qt.evolve_qtrait(rng,
-                            1,
+                            4,
                             1000,
                             nlist[0:],
                             0,
@@ -23,8 +23,20 @@ for i in range(2):
                             [fp.Region(0,1,1)],
                             0.1,
                             0.,
-                            track=1,trackStats=10000)
-    #Evolve another 10N gens after shift optimum to 0.25
+                            track=1,trackStats=0)
+    nlist = np.array([1000]*1000,dtype=np.uint32)
+    qt.evolve_qtrait_more(rng,pops,
+                          nlist[0:],
+                          0,
+                          0.01,
+                          0.001,
+                          [],
+                          [fp.GaussianS(0,1,1,0.25)],
+                          [fp.Region(0,1,1)],
+                          0.1,
+                          0.,
+                          track=1,trackStats=1)
+    #Evolve another N gens after shift optimum to 0.5
     qt.evolve_qtrait_more(rng,pops,
                             nlist[0:],
                             0,
@@ -34,19 +46,10 @@ for i in range(2):
                             [fp.GaussianS(0,1,1,0.25)],
                             [fp.Region(0,1,1)],
                             0.1,
-                            optimum=0.25,
-                            track=1,trackStats=2)
+                            optimum=0.5,
+                            track=1,trackStats=1)
     st=[qt.popstats(i) for i in pops]
-    print st[0]
-    #Get the mutation frequency trajectories, etc.
-    #for all mutations that ever happened
-    #tri is a list of pandas.DataFrame
-    tri=[fp.trajectories(p,10) for p in pops]
-    #Add a 'replicate ID' to each element in tri
-    for j in range(len(tri)):
-        tri[j]['rep']=[k]*len(tri[j].index)
-        k=k+1
-        #write each data frame to h5 file
-        hdf.append('tr',tri[j])
+    for i in st:
+        hdf.append('popstats',pandas.DataFrame(i))
 
 hdf.close()
