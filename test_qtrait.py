ab386a6c7977d67bc901a2f540ee0b85c458da93
Kevin R. Thornton <krthornt@uci.edu>
2016-02-16 15:01:55 -0800

    update function calls and output

--- test_qtrait.py
+++ test_qtrait.py
@@ -25,10 +25,9 @@ for i in range(500):
                             [fp.GaussianS(0,1,1,0.25)],
                             [fp.Region(0,1,1)],
                             sigE,
-                            0.,
-                            track=0,trackStats=0)
+                            0.)
     nlist = np.array([1000]*1000,dtype=np.uint32)
-    qt.evolve_qtrait_more(rng,pops,
+    st=qt.evolve_qtrait_popstats(rng,pops,
                           nlist[0:],
                           0,
                           mu,
@@ -37,23 +36,25 @@ for i in range(500):
                           [fp.GaussianS(0,1,1,0.25)],
                           [fp.Region(0,1,1)],
                           sigE,
-                          0.,
-                          track=0,trackStats=1)
+                                 trackStats=1,
+                          optimum=0.)
+    for j in st:
+        hdf.append('popstats',pandas.DataFrame(j))
     #Evolve another N gens after shift optimum to 0.5
     nlist = np.array([1000]*3000,dtype=np.uint32)
-    qt.evolve_qtrait_more(rng,pops,
-                          nlist[0:],
-                          0,
-                          mu,
-                          r,
-                          [],
-                          [fp.GaussianS(0,1,1,0.25)],
-                          [fp.Region(0,1,1)],
-                          sigE,
-                          optimum=0.5,
-                          track=0,trackStats=1)
-    st=[qt.popstats(i) for i in pops]
-    for i in st:
-        hdf.append('popstats',pandas.DataFrame(i))
+    st=qt.evolve_qtrait_popstats(rng,pops,
+                                 nlist[0:],
+                                 0,
+                                 mu,
+                                 r,
+                                 [],
+                                 [fp.GaussianS(0,1,1,0.25)],
+                                 [fp.Region(0,1,1)],
+                                 sigE,
+                                 1,
+                                optimum=0.5)
+    for j in st:
+        hdf.append('popstats',pandas.DataFrame(j))
+
 
 hdf.close()
