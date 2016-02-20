f5bc3b615ca629be07f8558ffdd25cc4c506bc4c
Kevin R. Thornton <krthornt@uci.edu>
2016-02-20 15:38:00 -0800

    definitions of demographic functions.  simpler implementation
    of re_init_mpop

--- fwdpy/fwdpy/metapop.cc
+++ fwdpy/fwdpy/metapop.cc
@@ -1,7 +1,9 @@
+#include <memory>
+#include <stdexcept>
 #include <gsl/gsl_randist.h>
 #include <types.hpp>
 #include <fwdpp/util.hpp>
-#include <memory>
+#include <fwdpp/sugar/demography.hpp>
 namespace fwdpy {
 
   size_t migpop(const size_t & source_pop, const gsl_rng * r, const double & mig_prob)
@@ -16,29 +18,47 @@ namespace fwdpy {
   void re_init_mpop( metapop_t * mpop,
 		     const singlepop_t * pop)
   {
-    mpop->diploids.clear();
-    mpop->gametes.clear();
-    mpop->Ns.clear();
-
-    mpop->fixations = pop->fixations;
-    mpop->fixation_times = pop->fixation_times;
-    mpop->mutations = pop->mutations;
-    mpop->gametes = pop->gametes;
-    mpop->diploids.emplace_back(pop->diploids);
-    mpop->mut_lookup = pop->mut_lookup;
-    mpop->Ns.push_back(unsigned(mpop->diploids[0].size()));
-    mpop->mcounts = pop->mcounts;
-    //Update other data
-    mpop->generation = pop->generation;
+    //use copy-construction from singlepop.
+    *mpop=metapop_t(*pop);
   }
 
-  //Make a copy of deme i
+  /*
+    Generic demographic operations.
+
+    Calls are made to fwdpp's sugar layer, which in turn 
+    calls lower-level functions.  
+
+    Any non-zero return value is treated as a run-time error
+    and an exception is thrown.
+   */
   void copy_deme( metapop_t * mpop,
 		  const size_t i )
   {
-    mpop->diploids.push_back(metapop_t::dipvector_t(mpop->diploids[i]));
-    mpop->Ns.push_back(unsigned(mpop->diploids[i].size()));
-    KTfwd::fwdpp_internal::process_gametes(mpop->gametes,mpop->mutations,mpop->mcounts);
+    int rv = KTfwd::copy_pop(*mpop,i);
+    if(rv) throw std::runtime_error("error copying deme");
   }
 
+  void remove_deme( metapop_t * mpop, const std::size_t i )
+  {
+    int rv=KTfwd::remove_pop(*mpop,i);
+    if(rv) throw std::runtime_error("error removing deme");
+  }
+  
+  void merge_demes(metapop_t  * mpop, const std::size_t i, const std::size_t j)
+  {
+    int rv=KTfwd::merge_pops(*mpop,i,j);
+    if(rv) throw std::runtime_error("error merging demes");
+  }
+  
+  void split_deme(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const unsigned N_new, const bool replacement )
+  {
+    int rv=KTfwd::split_pop(r,*mpop,i,N_new,replacement);
+    if(rv) throw std::runtime_error("error splitting deme");
+  }
+  
+  void admix_demes(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const std::size_t j, const double prop_i, const bool replacement)
+  {
+    int rv=KTfwd::admix_pops(r,*mpop,i,j,prop_i,replacement);
+    if(rv) throw std::runtime_error("error admixing demes");
+  }
 }
