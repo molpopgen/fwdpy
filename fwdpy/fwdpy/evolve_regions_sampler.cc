826b2273f3d9d6b5979485304e4605f815ae2e49
Kevin R. Thornton <krthornt@uci.edu>
2016-03-02 15:04:10 -0800

    indentation

--- fwdpy/fwdpy/evolve_regions_sampler.cc
+++ fwdpy/fwdpy/evolve_regions_sampler.cc
@@ -33,24 +33,24 @@ namespace fwdpy
 			       const unsigned nsam,
 			       const internal::region_manager * rm,
 			       const char * fitness)
-    {
-      return evolve_regions_async_wrapper<sample_n,decltype(nsam)>(rng,pops,Nvector,Nvector_len,mu_neutral,mu_selected,littler,f,sample,
-								   rm,fitness,std::forward<decltype(nsam)>(nsam));
-    }
+  {
+    return evolve_regions_async_wrapper<sample_n,decltype(nsam)>(rng,pops,Nvector,Nvector_len,mu_neutral,mu_selected,littler,f,sample,
+								 rm,fitness,std::forward<decltype(nsam)>(nsam));
+  }
 
-    std::vector<get_selected_mut_data::final_t>
-    evolve_regions_track_async( GSLrng_t * rng, std::vector<std::shared_ptr<singlepop_t> > * pops,
-    			       const unsigned * Nvector,
-    			       const size_t Nvector_len,
-    			       const double mu_neutral,
-    			       const double mu_selected,
-    			       const double littler,
-    			       const double f,
-    			       const int sample,
-    			       const internal::region_manager * rm,
-    			       const char * fitness)
-    {
-      return evolve_regions_async_wrapper<get_selected_mut_data>(rng,pops,Nvector,Nvector_len,mu_neutral,mu_selected,littler,f,sample,
-								 rm,fitness);
-    }
+  std::vector<get_selected_mut_data::final_t>
+  evolve_regions_track_async( GSLrng_t * rng, std::vector<std::shared_ptr<singlepop_t> > * pops,
+			      const unsigned * Nvector,
+			      const size_t Nvector_len,
+			      const double mu_neutral,
+			      const double mu_selected,
+			      const double littler,
+			      const double f,
+			      const int sample,
+			      const internal::region_manager * rm,
+			      const char * fitness)
+  {
+    return evolve_regions_async_wrapper<get_selected_mut_data>(rng,pops,Nvector,Nvector_len,mu_neutral,mu_selected,littler,f,sample,
+							       rm,fitness);
+  }
 }
