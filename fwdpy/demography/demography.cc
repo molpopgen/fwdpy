bbbafbffa20d3f2a17402bb44a42ea1a39833ba9
Kevin R. Thornton <krthornt@uci.edu>
2016-04-01 14:37:28 -0700

    file added with placeholder definitions

---
+++ fwdpy/demography/demography.cc
@@ -0,0 +1,31 @@
+namespace fwdpy
+{
+  size_t migpop(const size_t & source_pop, const gsl_rng * r, const double & mig_prob)
+  {
+  }
+
+  void re_init_mpop( metapop_t * mpop, const singlepop_t * pop)
+  {
+  }
+
+  /*
+    Generic demographic operations.
+    These functions are exposed to Cython as except + b/c their implementations
+    will throw std::runtime_error if the underying fwdpp function returns a non-zero value
+   */
+  void copy_deme( metapop_t * mpop, const std::size_t i )
+  {
+  }
+  void remove_deme( metapop_t * mpop, const std::size_t i )
+  {
+  }
+  void merge_demes(metapop_t  * mpop, const std::size_t i, const std::size_t j)
+  {
+  }
+  void split_deme(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const unsigned N_new, const bool replacement )
+  {
+  }
+  void admix_demes(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const std::size_t j, const double prop_i, const bool replacement)
+  {
+  }
+}
