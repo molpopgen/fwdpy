78ab03d13d343ac7e042b60e554fa172a1461069
Kevin R. Thornton <krthornt@uci.edu>
2016-04-01 14:43:19 -0700

    add implementations to most functions

--- fwdpy/demography/demography.cc
+++ fwdpy/demography/demography.cc
@@ -1,3 +1,5 @@
+#include "metapop.hpp"
+#include <fwdpp/sugar/demography.hpp>
 namespace fwdpy
 {
   size_t migpop(const size_t & source_pop, const gsl_rng * r, const double & mig_prob)
@@ -8,24 +10,28 @@ namespace fwdpy
   {
   }
 
-  /*
-    Generic demographic operations.
-    These functions are exposed to Cython as except + b/c their implementations
-    will throw std::runtime_error if the underying fwdpp function returns a non-zero value
-   */
   void copy_deme( metapop_t * mpop, const std::size_t i )
   {
+    KTfwd::copy_pop(*mpop,i);
   }
+  
   void remove_deme( metapop_t * mpop, const std::size_t i )
   {
+    KTfwd::remove_pop(*mpop,i);
   }
+  
   void merge_demes(metapop_t  * mpop, const std::size_t i, const std::size_t j)
   {
+    KTfwd::merge_pops(*mpop,i,j);
   }
+  
   void split_deme(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const unsigned N_new, const bool replacement )
   {
+    KTfwd::split_pop(r,*mpop,i,N_new,replacement);
   }
+  
   void admix_demes(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const std::size_t j, const double prop_i, const bool replacement)
   {
+    KTfwd::admix_pops(r,*mpop,i,j,prop_i,replacement);
   }
 }
