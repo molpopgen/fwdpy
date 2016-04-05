6058143c739c6d9e2d906527feb713a97204811d
Kevin R. Thornton <krthornt@uci.edu>
2016-04-05 11:15:17 -0700

    fix prototype for admixture

--- include/metapop.hpp
+++ include/metapop.hpp
@@ -26,7 +26,7 @@ namespace fwdpy
   void remove_deme( metapop_t * mpop, const std::size_t i );
   void merge_demes(metapop_t  * mpop, const std::size_t i, const std::size_t j);
   void split_deme(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const unsigned N_new, const bool replacement );
-  void admix_demes(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const std::size_t j, const double prop_i, const bool replacement);
+  void admix_demes(const gsl_rng * r, metapop_t * mpop, const std::size_t i, const std::size_t j, const double prop_i, unsigned N_new, const bool replacement);
   void swap_demes(metapop_t * mpop, const std::size_t i, const size_t j);
 }
 
