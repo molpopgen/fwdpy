9496e491371392d92ff8174d66ef63b6f133b5be
Kevin R. Thornton <krthornt@uci.edu>
2016-04-08 09:31:09 -0700

    add new set of sets.  move simulation to top to play role of common objects for testing

--- fwdpy/tests/test_sampling.py
+++ fwdpy/tests/test_sampling.py
@@ -3,6 +3,34 @@ import fwdpy as fp
 import numpy as np
 import pandas as pd
 
+##Run some quick sims that we use for tests below:
+
+nregions = [fp.Region(beg=0,end=1,weight=1)]
+
+sregions = [fp.ConstantS(beg=-1,end=0,weight=1,s=-0.05,h=1),
+            fp.ConstantS(beg=1,end=2,weight=1,s=-0.05,h=1)]
+
+recregions = [fp.Region(beg=-1,end=2,weight=1)]
+
+N=1000
+nlist = np.array([N]*10*N,dtype=np.uint32)
+
+rng = fp.GSLrng(101)
+
+pops = fp.evolve_regions(rng,       #The random number generator
+                         4,         #The number of pops to simulate = number of threads to use.
+                         N,         #Initial population size for each of the 4 demes
+                         nlist[0:], #List of population sizes over time.
+                         0.005,     #Neutral mutation rate (per gamete, per generation)
+                         0.01,      #Deleterious mutation rate (per gamete, per generation)
+                         0.005,     #Recombination rate (per diploid, per generation)
+                         nregions,  #Defined above
+                         sregions,  #Defined above
+                         recregions)#Defined above
+##Get fixation views:
+fixations = [fp.view_fixations(i) for i in pops]
+##Get mutation views:
+mviews = fp.view_mutations(pops)
 
 class test_sample_details(unittest.TestCase):
     def test_CompareUsingView(self):
@@ -12,35 +40,11 @@ class test_sample_details(unittest.TestCase):
         those details match up what 'views' think should
         be in the population.
         """
-        nregions = [fp.Region(beg=0,end=1,weight=1)]
-
-        sregions = [fp.ConstantS(beg=-1,end=0,weight=1,s=-0.05,h=1),
-                    fp.ConstantS(beg=1,end=2,weight=1,s=-0.05,h=1)]
-
-        recregions = [fp.Region(beg=-1,end=2,weight=1)]
-
-        N=1000
-        nlist = np.array([N]*10*N,dtype=np.uint32)
-
-        rng = fp.GSLrng(101)
-
-        pops = fp.evolve_regions(rng,       #The random number generator
-                                 4,         #The number of pops to simulate = number of threads to use.
-                                 N,         #Initial population size for each of the 4 demes
-                                 nlist[0:], #List of population sizes over time.
-                                 0.005,     #Neutral mutation rate (per gamete, per generation)
-                                 0.01,      #Deleterious mutation rate (per gamete, per generation)
-                                 0.005,     #Recombination rate (per diploid, per generation)
-                                 nregions,  #Defined above
-                                 sregions,  #Defined above
-                                 recregions)#Defined above
-
         samples = [fp.get_samples(rng,i,100) for i in pops]
         
         details = [fp.get_sample_details(i[1],j) for i,j in zip(samples,pops)]
 
-        ##Get mutation views
-        mviews = fp.view_mutations(pops)
+
 
         ##For each element in details,
         ##find it in the "mutation view" for that replicate
@@ -58,6 +62,83 @@ class test_sample_details(unittest.TestCase):
                     self.assertEqual(mmi['s'],details[i].s[j])
                     EP=mmi['n']/float(2000) #"expected" frequency
                     self.assertEqual(EP,details[i].p[j])
-            
+
+class test_Fixations(unittest.TestCase):
+    def test_CountFixationsInSample1(self):
+        """
+        Evolve some pops, take samples w/o removing fixations,
+        Make sure that all mutations in a view of fixations
+        are also present in the sample.
+
+        Ultimately, this is just a test of whether or not
+        fwdpy passes the correct args to fwdpp.
+        """
+        #False = DO NOT remove fixed variants
+        samples = [fp.ms_sample(rng,i,100,False) for i in pops] #This fxn merges all selected + neutral variants into 1 object per replicate
+    
+
+        #for each collection of fixations...
+        for i in range(len(fixations)):
+            #...go thru each fixation...
+            FOUND=0
+            for f in fixations[i]:
+                #...find it in the sample object for the correct replicate...
+                mm=[X for X in samples[i] if X[0] == f[1]['pos']]
+                FOUND+=len(mm)
+            self.assertEqual(FOUND,len(fixations[i]))
+    def test_CountFixationsInSample2(self):
+        #Repeat above test, but using sampler where neutral + selected variants are in different objects
+        samples = [fp.get_samples(rng,i,100,False) for i in pops]
+
+        for i in range(len(fixations)):
+            FOUNDN=0
+            FOUNDS=0
+            for f in fixations[i]:
+                if f[1]['neutral'] is True:
+                    mm=[X for X in samples[i][0] if X[0]==f[1]['pos']]
+                    FOUNDN+=len(mm)
+                else:
+                    mm=[X for X in samples[i][1] if X[0]==f[1]['pos']]
+                    FOUNDS+=len(mm)
+                    self.assertEqual(FOUNDN+FOUNDS,len(fixations[i]))
+    def test_CountFixationsInSample3(self):
+        #Finally, make sure that sample w/o including fixations works,
+        #meaning that no mutations are present in the sample with
+        #derived count == nsam
+        samples = [fp.get_samples(rng,i,100) for i in pops]
+        ##Checks that no "true fixations" are in vector
+        for i in range(len(fixations)):
+            FOUNDN=0
+            FOUNDS=0
+            for f in fixations[i]:
+                if f[1]['neutral'] is True:
+                    mm=[X for X in samples[i][0] if X[0]==f[1]['pos']]
+                    FOUNDN+=len(mm)
+                else:
+                    mm=[X for X in samples[i][1] if X[0]==f[1]['pos']]
+                    FOUNDS+=len(mm)
+                    self.assertEqual(FOUNDN+FOUNDS,0)
+        ##Checks that no polymorphisms are present as fixations in sample
+        for i in samples:
+            for j in i:
+                x=j[0][1].count('1')
+                self.assertTrue(x<100)
+                x=j[1][1].count('1')
+                self.assertTrue(x<100)
+                
+    def test_CountFixationsInSample4(self):
+        samples = [fp.ms_sample(rng,i,100) for i in pops]
+        ##Checks that no "true fixations" are in vector
+        for i in range(len(fixations)):
+            FOUND=0
+            for f in fixations[i]:
+                mm=[X for X in samples[i] if X[0] == f[1]['pos']]
+                FOUND+=len(mm)
+                self.assertEqual(FOUND,0)
+        ##Checks that no polymorphisms are present as fixations in sample
+        for i in samples:
+            for j in i:
+                self.assertTrue(j[1].count('1')<100)
+                
 if __name__ == '__main__':
     unittest.main()
