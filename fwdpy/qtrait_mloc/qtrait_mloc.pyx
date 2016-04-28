f4a646f92ea171df311337543639675fac58a14b
Kevin R. Thornton <krthornt@uci.edu>
2016-04-28 10:06:35 -0700

    add wrappers

--- fwdpy/qtrait_mloc/qtrait_mloc.pyx
+++ fwdpy/qtrait_mloc/qtrait_mloc.pyx
@@ -13,6 +13,21 @@ import fwdpy.internal as internal
 import pandas
 
 cdef extern from "qtrait_evolve_mlocus.hpp" namespace "fwdpy::qtrait" nogil:
+     void evolve_qtrait_mloc_no_sampling_async( GSLrng_t * rng,
+				                vector[shared_ptr[multilocus_t]] * pops,
+				                const unsigned * Nvector,
+				                const size_t Nvector_length,
+				                const vector[double] & neutral_mutation_rates,
+				                const vector[double] & selected_mutation_rates,
+				                const vector[double] & sigma_mus,
+				                const vector[double] & within_region_rec_rates,
+				                const vector[double] & between_region_rec_rates,
+				                const double f,
+				                const double sigmaE,
+				                const double optimum,
+				                const double VS,
+                                                const unsigned nsam) except +
+     
      vector[vector[pair[uint,vector[detailed_deme_sample]]]] evolve_qtrait_mloc_sample_async( GSLrng_t * rng,
 				                                                              GSLrng_t * rng_sample,
 				                                                              vector[shared_ptr[multilocus_t]] * pops,
@@ -29,5 +44,41 @@ cdef extern from "qtrait_evolve_mlocus.hpp" namespace "fwdpy::qtrait" nogil:
 				                                                              const double VS,
 				                                                              const int sample,
                                                                                               const unsigned nsam) except +
+
+     vector[vector[qtrait_stats_cython]] evolve_qtrait_mloc_popstats_async( GSLrng_t * rng,
+				                                            vector[shared_ptr[multilocus_t]] * pops,
+				                                            const unsigned * Nvector,
+				                                            const size_t Nvector_length,
+				                                            const vector[double] & neutral_mutation_rates,
+				                                            const vector[double] & selected_mutation_rates,
+				                                            const vector[double] & sigma_mus,
+				                                            const vector[double] & within_region_rec_rates,
+				                                            const vector[double] & between_region_rec_rates,
+				                                            const double f,
+				                                            const double sigmaE,
+				                                            const double optimum,
+				                                            const double VS,
+				                                            const int sample,
+                                                                            const unsigned nsam) except +
+
+     vector[vector[pair[selected_mut_data,vector[double]]]] evolve_qtrait_mloc_track_async( GSLrng_t * rng,
+				                                                            vector[shared_ptr[multilocus_t]] * pops,
+				                                                            const unsigned * Nvector,
+				                                                            const size_t Nvector_length,
+				                                                            const vector[double] & neutral_mutation_rates,
+				                                                            const vector[double] & selected_mutation_rates,
+				                                                            const vector[double] & sigma_mus,
+				                                                            const vector[double] & within_region_rec_rates,
+				                                                            const vector[double] & between_region_rec_rates,
+				                                                            const double f,
+				                                                            const double sigmaE,
+				                                                            const double optimum,
+				                                                            const double VS,
+				                                                            const int sample,
+                                                                                            const unsigned nsam) except +
+
+     
+
+     
 
 include "evolve_qtraits_mloc.pyx"
