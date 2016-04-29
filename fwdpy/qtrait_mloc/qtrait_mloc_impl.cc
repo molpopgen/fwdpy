9751597be5cc6400c301420e7e9c70bb5438ad3a
Kevin R. Thornton <krthornt@uci.edu>
2016-04-29 15:04:04 -0700

    new definition

--- fwdpy/qtrait_mloc/qtrait_mloc_impl.cc
+++ fwdpy/qtrait_mloc/qtrait_mloc_impl.cc
@@ -160,5 +160,37 @@ namespace fwdpy
 								  sample,
 								  rules);
     }
+
+    std::vector<additive_variance::final_t>
+    evolve_qtrait_mloc_VA_async( GSLrng_t * rng,
+				 std::vector<std::shared_ptr<multilocus_t> > * pops,
+				 const unsigned * Nvector,
+				 const size_t Nvector_length,
+				 const std::vector<double> & neutral_mutation_rates,
+				 const std::vector<double> & selected_mutation_rates,
+				 const std::vector<double> & sigma_mus,
+				 const std::vector<double> & within_region_rec_rates,
+				 const std::vector<double> & between_region_rec_rates,
+				 const double f,
+				 const double sigmaE,
+				 const double optimum,
+				 const double VS,
+				 const int sample)
+    {
+      qtrait_mloc_rules rules(sigmaE,optimum,VS,*std::max_element(Nvector,Nvector+Nvector_length));
+      return  evolve_qtrait_mloc_async_wrapper<fwdpy::additive_variance,
+					       qtrait_mloc_rules>(rng,
+								  pops,
+								  Nvector,
+								  Nvector_length,
+								  neutral_mutation_rates,
+								  selected_mutation_rates,
+								  sigma_mus,
+								  within_region_rec_rates,
+								  between_region_rec_rates,
+								  f,
+								  sample,
+								  rules);
+    }
   }
 }
