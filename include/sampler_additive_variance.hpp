0203d490587630e61e07e902b37299c80b6466dc
Kevin R. Thornton <krthornt@uci.edu>
2016-05-01 19:49:51 -0700

    added regression_details to do the nasty work.  This is much closer to a correct implementation now...

--- include/sampler_additive_variance.hpp
+++ include/sampler_additive_variance.hpp
@@ -58,6 +58,19 @@ namespace fwdpy
   using gsl_vector_ptr_t = std::unique_ptr< gsl_vector, gsl_vector_deleter >;
   using gsl_matrix_ptr_t = std::unique_ptr< gsl_matrix, gsl_matrix_deleter >;
 
+  struct regression_results
+  {
+    gsl_vector_ptr_t sums;
+    std::vector<std::size_t> ucol_labels;
+    std::size_t DF;
+    regression_results(gsl_vector_ptr_t && s, std::vector<std::size_t> && p, std::size_t df) :
+      sums(std::move(s)),
+      ucol_labels(std::move(p)),
+      DF(df)
+    {
+    }
+  };
+
   struct additive_variance
   {
     using final_t = std::vector<VGdata>;
@@ -67,60 +80,50 @@ namespace fwdpy
 			   unsigned generation)
     {
       if(generation%100==0.) std::cerr << generation << '\n';
+      auto mut_keys = get_mut_keys(pop);
+      if(mut_keys.empty())
+	{
+	  //TODO: add an empty thing to the return value.
+	  return;
+	}
       //Genetic values for each diploid
       double VG;
       auto G = fillG(pop,&VG);
-      auto mut_keys = get_mut_keys(pop);
       //0,1,2 count of mutations affecting fitness
       auto genotypes = make_variant_matrix(pop,mut_keys);
-
-      std::size_t NROW = pop->diploids.size();
-      std::size_t NCOL = genotypes->size2; //Neat...
-      //Allocate placeholder variables
-      gsl_vector_ptr_t tau(gsl_vector_alloc(NCOL));
-      gsl_vector_ptr_t sums(gsl_vector_alloc(NROW));
-      gsl_vector_ptr_t SS(gsl_vector_alloc(NROW));
-      gsl_matrix_ptr_t Q(gsl_matrix_alloc(NROW,NROW));
-      gsl_matrix_ptr_t R(gsl_matrix_alloc(NROW,NCOL));
-
-      //QR decomposition...
-      gsl_linalg_QR_decomp(genotypes.get(), tau.get());
-      //Get the Q and R matrix separately
-      gsl_linalg_QR_unpack(genotypes.get(), tau.get(), Q.get(), R.get());
-      //Multiply t(Q) %*% b and store in sums
-      gsl_blas_dgemv(CblasTrans, 1.0, Q.get(), G.get(), 0.0, sums.get());
-
-      //square the sums
+      //Get a vector of the mcounts corresponding to mut_kets
+      std::vector<KTfwd::uint_t> mut_key_counts;
+      for( const auto i : mut_keys ) mut_key_counts.emplace_back(pop->mcounts[i]);
+      
+      auto results = regression_details(G,genotypes,mut_keys,mut_key_counts);
       double SumOfSquares=0.0;
       std::vector<double> vSumOfSquares;
-      for(std::size_t i = 0; i < NROW; i++)
-	{
-	  auto s = gsl_vector_get(sums.get(),i);
-	  auto p = gsl_sf_pow_int(s,2);
-	  if(i)
-	    {
-	      vSumOfSquares.push_back(p);
-	      SumOfSquares+=p;
-	    }
-
-	  //gsl_vector_set(SS.get(), i, p);
-	}
-      double VGcheck=0.0;
-      for( std::size_t i=1 ; i < mut_keys.size() ; ++i )
-	{
-	  double pSS=vSumOfSquares[i]/SumOfSquares;
-	  VGcollection.emplace_back(pop->mutations[mut_keys[i]].pos,
-				    pop->mutations[mut_keys[i]].s,
-				    pSS*VG,
-				    vSumOfSquares[i],
-				    pSS,
-				    double(pop->mcounts[mut_keys[i]])/(2.0*double(pop->diploids.size())),
-				    pop->mutations[mut_keys[i]].g,
-				    generation
-				    );
-	  VGcheck+=pSS*VG;
-	}
-      std::cerr << VGcheck << ' ' << VG << ' ' << ' ' << SumOfSquares << '\n';
+      for(std::size_t i = 1; i < genotypes->size1 ; i++)
+      	{
+      	  auto s = gsl_vector_get(results.sums.get(),i);
+      	  auto p = gsl_sf_pow_int(s,2);
+      	  if(i)
+      	    {
+      	      vSumOfSquares.push_back(p);
+      	      SumOfSquares+=p;
+      	    }
+      	}
+      // double VGcheck=0.0;
+      // for( std::size_t i=1 ; i < mut_keys.size() ; ++i )
+      // 	{
+      // 	  double pSS=vSumOfSquares[i]/SumOfSquares;
+      // 	  VGcollection.emplace_back(pop->mutations[mut_keys[i]].pos,
+      // 				    pop->mutations[mut_keys[i]].s,
+      // 				    pSS*VG,
+      // 				    vSumOfSquares[i],
+      // 				    pSS,
+      // 				    double(pop->mcounts[mut_keys[i]])/(2.0*double(pop->diploids.size())),
+      // 				    pop->mutations[mut_keys[i]].g,
+      // 				    generation
+      // 				    );
+      // 	  VGcheck+=pSS*VG;
+      // 	}
+      // std::cerr << VGcheck << ' ' << VG << ' ' << ' ' << SumOfSquares << '\n';
     }
 
     final_t final() const
@@ -133,6 +136,98 @@ namespace fwdpy
     }
   private:
     final_t VGcollection;
+
+    regression_results regression_details(const gsl_vector_ptr_t & G,
+					  const gsl_matrix_ptr_t & genotypes,
+					  const std::vector<size_t> & mut_keys,
+					  const std::vector<KTfwd::uint_t> & mut_key_counts)
+    /*!
+      Handles ugly details of the regression:
+      1. Reduces genotypes just to the set of unique columns
+      2. Does the QR regression
+      3. Returns stuff
+
+      \note The indexing in this code is tricky, and could be improved via a GSL matrix view skipping 1st column of genotypes.
+     */
+    {
+      std::vector<size_t> column_labels(mut_keys.size());
+      std::size_t LAB=0;
+      std::generate(column_labels.begin(),column_labels.end(),[&LAB](){return LAB++;});
+      
+      unsigned identical = 0;
+      for( std::size_t col = 1 ; col < genotypes->size2-1 ; ++col ) //skip column 0...
+	{
+	  //gsl_vector_view col_i = gsl_matrix_column(genotypes,col);
+	  for( std::size_t col2 = col+1 ; col2 < genotypes->size2 ; ++col2 )
+	    {
+	      //columns can only be identical if mutations have same frequency!
+	      //The -1 is b/c genotypes has 1 extra columns
+	      if(mut_key_counts[col-1]==mut_key_counts[col2-1]) 
+		{
+		  unsigned ndiff=0;
+		  for( std::size_t row = 0 ; row < genotypes->size1 ; ++row )
+		    {
+		      if(gsl_matrix_get(genotypes.get(),row,col) != gsl_matrix_get(genotypes.get(),row,col2))
+			++ndiff;
+		    }
+		  if(!ndiff)
+		    {
+		      ++identical;
+		      column_labels[col2-1]=col-1; //This marks col2 as non-unique.
+		    }
+		}
+	    }
+	}
+      std::size_t NROW = genotypes->size1;
+      std::size_t NCOL = genotypes->size2-identical;
+      //Allocate placeholder variables
+      gsl_vector_ptr_t tau(gsl_vector_alloc(NCOL));
+      gsl_vector_ptr_t sums(gsl_vector_alloc(NROW));
+      gsl_matrix_ptr_t Q(gsl_matrix_alloc(NROW,NROW));
+      gsl_matrix_ptr_t R(gsl_matrix_alloc(NROW,NCOL));
+
+      if (identical)
+	{
+	  //Make new matrix of unique columns.
+	  gsl_matrix_ptr_t ugeno(gsl_matrix_alloc(genotypes->size1,genotypes->size2-identical));
+	  for(std::size_t i=0,j=0;i<genotypes->size2;++i)
+	    {
+	      //The -1 is b/c of the different lengths, as above...
+	      if(i==0 ||(column_labels[i-1]==i-1)) //Column is either column 0 or the column is unique
+		{
+		  for(std::size_t k = 0 ; k < genotypes->size1 ; ++k )
+		    {
+		      gsl_matrix_set(ugeno.get(),k,j,gsl_matrix_get(genotypes.get(),k,i));
+		    }
+		  ++j;
+		}
+	    }
+	  //QR decomposition...
+	  gsl_linalg_QR_decomp(ugeno.get(), tau.get());
+	  //Get the Q and R matrix separately
+	  gsl_linalg_QR_unpack(ugeno.get(), tau.get(), Q.get(), R.get());
+	  //Multiply t(Q) %*% b and store in sums
+	  gsl_blas_dgemv(CblasTrans, 1.0, Q.get(), G.get(), 0.0, sums.get());
+	  return regression_results(std::move(sums),
+				    std::move(column_labels),
+				    //This next value is the # of elements at end of sums
+				    //from which RSS can be calculates
+				    sums->size-NCOL-identical);
+	}
+
+      //QR decomposition...
+      gsl_linalg_QR_decomp(genotypes.get(), tau.get());
+      //Get the Q and R matrix separately
+      gsl_linalg_QR_unpack(genotypes.get(), tau.get(), Q.get(), R.get());
+      //Multiply t(Q) %*% b and store in sums
+      gsl_blas_dgemv(CblasTrans, 1.0, Q.get(), G.get(), 0.0, sums.get());
+      return regression_results(std::move(sums),
+				std::move(column_labels),
+				//This next value is the # of elements at end of sums
+				//from which RSS can be calculates
+				sums->size-NCOL-identical);
+    }
+    
     template<typename pop_t>
     std::vector<std::size_t> get_mut_keys(const pop_t * pop)
     {
