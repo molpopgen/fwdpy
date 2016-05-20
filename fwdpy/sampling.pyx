87a1be5339bec143c3812af8c046b061ee5d3bc3
Kevin R. Thornton <krthornt@uci.edu>
2016-05-20 14:40:50 -0700

    use C++ back-end instead of Cython

--- fwdpy/sampling.pyx
+++ fwdpy/sampling.pyx
@@ -300,17 +300,7 @@ def hapmatrix(p,const vector[size_t] & diploids, deme = None):
     else:
         raise RuntimeError("object type not understood")
 
-cdef row_col_indexes(list indexes,
-                     size_t ncol):
-    rv=[]
-    for i in indexes:
-        t=float(i+1)/float(ncol)
-        row=np.floor(t)
-        col=(t-row)*float(ncol)-1.0
-        rv.append((int(row),int(col)))
-    return rv
-
-def genomatrix(dict m):
+def genomatrix(const haplotype_matrix & m):
     """
     Generate a "genotype matrix" from a haplotype matrix
 
@@ -322,39 +312,4 @@ def genomatrix(dict m):
     ..note :: The return value is most useful when used to create a 2d numpy array representing 
     genotypes.
     """
-
-    #Step 1: get the row/column index of each mutation in the haplotype matrix
-    n=row_col_indexes(m['n'],m['ncol_n'])
-    s=row_col_indexes(m['s'],m['ncol_s'])
-
-    #Step 2: For each pair of rows, collect Aa and aa genotypes
-    nAa=[]
-    naa=[]
-    sAa=[]
-    saa=[]
-    newrows=m['nrow']/2
-    newrow=0
-    for row in range(0,m['nrow'],2):
-        #Get data for this and the next row in the hap matrix
-        x=[i[1] for i in n if (i[0]==row or i[0]==(row+1))]
-        xu=set(x)
-        for i in xu:
-            ci=x.count(i)
-            if ci == 1:
-                nAa.append(newrow*m['ncol_n']+i)
-            elif ci == 2:
-                naa.append(newrow*m['ncol_n']+i)
-
-        #do same for selected variants
-        x=[i[1] for i in s if (i[0]==row or i[0]==(row+1))]
-        xu=set(x)
-        for i in xu:
-            ci=x.count(i)
-            if ci == 1:
-                sAa.append(newrow*m['ncol_s']+i)
-            elif ci == 2:
-                saa.append(newrow*m['ncol_s']+i)
-
-        newrow+=1
-
-    return {'nAa':nAa,'naa':naa,'sAa':sAa,'saa':saa}
+    return make_genotype_matrix(m);
