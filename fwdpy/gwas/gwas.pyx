0ade976d8f706fd6a598f40524f4cb83d77f7fd8
Kevin R. Thornton <krthornt@uci.edu>
2016-06-03 10:02:56 -0700

    define new Python function (API likely to change, etc.)

--- fwdpy/gwas/gwas.pyx
+++ fwdpy/gwas/gwas.pyx
@@ -1,4 +1,5 @@
 from fwdpy.fwdpy cimport popvec,singlepop,singlepop_t
+from fwdpy.samplers cimport additive_variance_wrapper
 
 def genotype_matrices(popvec pops):
     rv=[]
@@ -6,3 +7,10 @@ def genotype_matrices(popvec pops):
         temp=make_geno_matrix(pops.pops[i].get(),True)
         rv.append(temp)
     return rv
+
+def VAVG(object p):
+    if isinstance(p,singlepop):
+        #Return element 0, as there is no need to return a vector, as not running sampler over time...
+        return additive_variance_wrapper[singlepop_t]((<singlepop>p).pop.get())[0]
+    elif isinstance(p,popvec):
+        return [additive_variance_wrapper[singlepop_t]((<singlepop>i).pop.get())[0] for i in popvec]
