3f20df9679a53dabf12cae1c90cac0e1f289fd9f
Kevin R. Thornton <krthornt@uci.edu>
2016-06-16 12:17:24 -0700

    added new test evolve function

--- include/evolve_regions_sampler.hpp
+++ include/evolve_regions_sampler.hpp
@@ -1,6 +1,6 @@
 #ifndef FWDPY_EVOLVE_REGIONS_SAMPLER_HPP
 #define FWDPY_EVOLVE_REGIONS_SAMPLER_HPP
-
+#include <thread>
 #include <future>
 #include <vector>
 #include <algorithm>
@@ -15,6 +15,7 @@
 #include "sampler_pop_properties.hpp"
 #include "sampler_sample_n.hpp"
 #include "sampler_selected_mut_tracker.hpp"
+#include "sampler_base.hpp"
 #include "metaprogramming.hpp"
 
 namespace fwdpy
@@ -194,7 +195,7 @@ namespace fwdpy
 					const char * fitness);
   
   //Prototypes for functions using samplers
-  std::vector<sample_n<singlepop_t>::final_t>
+  std::vector<sample_n::final_t>
   evolve_regions_sample_async( GSLrng_t * rng,         //To evolve the populations
 			       GSLrng_t * rng_sampling,//To take the random samples of size nsam
 			       std::vector<std::shared_ptr<singlepop_t> > * pops,
@@ -222,5 +223,135 @@ namespace fwdpy
 			      const internal::region_manager * rm,
 			      const char * fitness);
 
+  //Experimental
+
+  inline void evolve_regions_sampler_test_details(singlepop_t * pop,
+						  const unsigned long seed,
+						  const unsigned * Nvector,
+						  const size_t Nvector_len,
+						  const double neutral,
+						  const double selected,
+						  const double recrate,
+						  const double f,
+						  const char * fitness,
+						  const int interval,
+						  KTfwd::extensions::discrete_mut_model && __m,
+						  KTfwd::extensions::discrete_rec_model && __recmap,
+						  sampler_base & s)
+  {
+    const size_t simlen = Nvector_len;
+    auto x = std::max_element(Nvector,Nvector+Nvector_len);
+    assert(x!=Nvector+Nvector_len);
+    reserve_space(pop->gametes,pop->mutations,*x,neutral+selected);
+    const double mu_tot = neutral + selected;
+    gsl_rng * rng = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(rng,seed);
+    KTfwd::extensions::discrete_mut_model m(std::move(__m));
+    KTfwd::extensions::discrete_rec_model recmap(std::move(__recmap));
+    //Recombination policy: more complex than the standard case...
+    const auto recpos = KTfwd::extensions::bind_drm(recmap,pop->gametes,pop->mutations,
+						    rng,recrate);
+
+    /*
+      The fitness model.
+
+      Normally, we'd declare dipfit as "auto", but that won't work here b/c there is the chance
+      that we have to re-assign it using an additive model based on input from calling environment.
+
+      The std::bind signature has a different type for the two models, and thus we must coerce it to
+      the proper function signature, which is a member typedef provided by the fwdpp sugar type
+      from which fwdpy::singlepop_t inherits
+    */
+    fwdpy::singlepop_t::fitness_t dipfit = std::bind(KTfwd::multiplicative_diploid(),
+						     std::placeholders::_1,
+						     std::placeholders::_2,
+						     std::placeholders::_3,
+						     2.);
+
+    if( std::string(fitness) == "additive" )
+      {
+	dipfit = std::bind(KTfwd::additive_diploid(),
+			   std::placeholders::_1,
+			   std::placeholders::_2,
+			   std::placeholders::_3,
+			   2.);
+      }
+
+    //create the sampler
+    //sampler s(std::forward<sampler>(isampler));
+    
+    for( size_t g = 0 ; g < simlen ; ++g, ++pop->generation )
+      {
+	const unsigned nextN = 	*(Nvector+g);
+	KTfwd::sample_diploid(rng,
+			      pop->gametes,
+			      pop->diploids,
+			      pop->mutations,
+			      pop->mcounts,
+			      pop->N,
+			      nextN,
+			      mu_tot,
+			      KTfwd::extensions::bind_dmm(m,pop->mutations,pop->mut_lookup,rng,neutral,selected,pop->generation),
+			      recpos,
+			      dipfit,
+			      pop->neutral,pop->selected,
+			      f);
+	pop->N=nextN;
+	if (interval && pop->generation &&(pop->generation+1)%interval==0.)
+	  {
+	    s(pop,pop->generation+1);
+	  }
+	KTfwd::update_mutations(pop->mutations,pop->fixations,pop->fixation_times,pop->mut_lookup,pop->mcounts,pop->generation,2*nextN);
+	assert(KTfwd::check_sum(pop->gametes,2*nextN));
+      }
+    //Update population's size variable to be the current pop size
+    pop->N = unsigned(pop->diploids.size());
+    //cleanup
+    gsl_rng_free(rng);
+  }
+  
+  
+  inline void evolve_regions_sampler_test( GSLrng_t * rng,
+					   std::vector<std::shared_ptr<singlepop_t> > * pops,
+					   std::vector<std::unique_ptr<sampler_base> > & samplers,
+					   const unsigned * Nvector,
+					   const size_t Nvector_length,
+					   const double mu_neutral,
+					   const double mu_selected,
+					   const double littler,
+					   const double f,
+					   const int sample,
+					   const internal::region_manager * rm,
+					   const char * fitness)
+  {
+    //check inputs--this is point of failure.  Throw excceptions here b4 getting into any threaded nonsense.
+    if(mu_neutral < 0. || mu_selected < 0. || littler < 0.)
+      {
+	throw std::runtime_error("mutation and recombination rates must all be non-negative.");
+      }
+    if(f<0.||f>1.) throw std::runtime_error("selfing probabilty must be 0<=f<=1.");
+    if(sample<0) throw std::runtime_error("sampling interval must be non-negative");
+    std::string ftnss(fitness);
+    if(ftnss!="additive"&&ftnss!="multiplicative")
+      {
+	throw std::runtime_error("fitness model must be additive or multiplicative");
+      }
+    //std::vector<future_t> futures;
+    std::vector<std::thread> threads;
+    for(std::size_t i=0;i<pops->size();++i)
+      {
+	threads.emplace_back( std::thread(//std::launch::async,
+					 evolve_regions_sampler_test_details,
+					 pops->operator[](i).get(),gsl_rng_get(rng->get()),Nvector,Nvector_length,
+					 mu_neutral,mu_selected,littler,f,fitness,sample,
+					 std::move(KTfwd::extensions::discrete_mut_model(rm->nb,rm->ne,rm->nw,rm->sb,rm->se,rm->sw,rm->callbacks)),
+					 std::move(KTfwd::extensions::discrete_rec_model(rm->rb,rm->rw,rm->rw)),
+					 std::ref(*samplers[i])
+					 )
+			      );	
+      }
+    //for(std::size_t i=0;i<futures.size();++i) futures[i].get();
+    for(std::size_t i=0;i<threads.size();++i) threads[i].join();
+  }
 } //ns fwdpy
 #endif
