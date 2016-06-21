8d6a6c4c9fe353edede3a510c66f5f18b79b16a4
molpopgen <krthornt@uci.edu>
2016-06-21 07:31:33 -0500

    shorten run time.  fix type of fitness model.

--- extension_tests/run_test_extensions/run_custom_fitness.py
+++ extension_tests/run_test_extensions/run_custom_fitness.py
@@ -7,7 +7,7 @@ rngs=fp.GSLrng(202)
 p = fp.popvec(3,1000)
 s = fp.nothing_sampler(len(p))
 
-n=np.array([1000]*10000,dtype=np.uint32)
+n=np.array([1000]*1000,dtype=np.uint32)
 nr=[fp.Region(0,1,1)]
 sr=[fp.ExpS(0,1,1,0.1)]
 
@@ -15,5 +15,8 @@ fitness = tfp.additiveFitnessTesting()
 fp.evolve_regions_sampler_fitness(rng,p,s,fitness,n,0.001,0.001,0.001,nr,sr,nr,1)
 
 fitness = tfp.Aa_only_testing()
+fp.evolve_regions_sampler_fitness(rng,p,s,fitness,n,0.001,0.001,0.001,nr,sr,nr,1)
+
+fitness = tfp.GBRfitness()
 fp.evolve_regions_sampler_fitness(rng,p,s,fitness,n,0.001,0.001,0.001,nr,sr,nr,1)
 
