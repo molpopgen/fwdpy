ba7ef9c8c59fad18fe56aa5fb26e53a7e71c2ef4
molpopgen <krthornt@uci.edu>
2016-06-20 19:26:24 -0500

    moved stuff to the new pxd

--- extension_tests/test_fwdpy_extensions/test_custom_fitness.pyx
+++ extension_tests/test_fwdpy_extensions/test_custom_fitness.pyx
@@ -1,11 +1,4 @@
-from fwdpy.fitness cimport singlepopFitness,genotype_fitness_updater,fitness_function_finalizer,make_custom_fitness,return_w_plus1
-from fwdpy.fwdpp cimport popgenmut
-
-cdef void additive_het_testing(double & w, const popgenmut & m):
-    (&w)[0]+=m.s*m.h
-
-cdef void additive_hom_testing(double & w, const popgenmut & m):
-    (&w)[0]+=m.s*2.0
+from fwdpy.fitness cimport genotype_fitness_updater,fitness_function_finalizer,make_custom_fitness,return_w_plus1
 
 cdef class additiveFitnessTesting(singlepopFitness):
     def __cinit__(self):
@@ -13,12 +6,6 @@ cdef class additiveFitnessTesting(singlepopFitness):
                                         <genotype_fitness_updater>additive_hom_testing,
                                         <fitness_function_finalizer>return_w_plus1,
                                         0.0)
-
-cdef void Aa_only_het_testing(double & w, const popgenmut & m):
-    (&w)[0] += m.s*m.h
-
-cdef void Aa_only_hom_testing(double & w, const popgenmut & m):
-    return
 
 cdef class AA_only_testing(singlepopFitness):
     def __cinit__(self):
