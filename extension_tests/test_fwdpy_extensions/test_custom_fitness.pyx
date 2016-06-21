51aaaf5bdfbb03267a54dadfee4535732c66359a
molpopgen <krthornt@uci.edu>
2016-06-20 19:11:38 -0500

    update to use built-in finalizer

--- extension_tests/test_fwdpy_extensions/test_custom_fitness.pyx
+++ extension_tests/test_fwdpy_extensions/test_custom_fitness.pyx
@@ -1,4 +1,4 @@
-from fwdpy.fitness cimport singlepopFitness,genotype_fitness_updater,fitness_function_finalizer,make_custom_fitness
+from fwdpy.fitness cimport singlepopFitness,genotype_fitness_updater,fitness_function_finalizer,make_custom_fitness,return_w_plus1
 from fwdpy.fwdpp cimport popgenmut
 
 cdef void additive_het_testing(double & w, const popgenmut & m):
@@ -7,14 +7,11 @@ cdef void additive_het_testing(double & w, const popgenmut & m):
 cdef void additive_hom_testing(double & w, const popgenmut & m):
     (&w)[0]+=m.s*2.0
 
-cdef double additive_finalize_testing(const double w):
-    return 1.0 + w
-
 cdef class additiveFitnessTesting(singlepopFitness):
     def __cinit__(self):
         self.wfxn = make_custom_fitness(<genotype_fitness_updater>additive_het_testing,
                                         <genotype_fitness_updater>additive_hom_testing,
-                                        <fitness_function_finalizer>additive_finalize_testing,
+                                        <fitness_function_finalizer>return_w_plus1,
                                         0.0)
 
 cdef void Aa_only_het_testing(double & w, const popgenmut & m):
@@ -27,5 +24,5 @@ cdef class AA_only_testing(singlepopFitness):
     def __cinit__(self):
         self.wfxn = make_custom_fitness(<genotype_fitness_updater>Aa_only_het_testing,
                                         <genotype_fitness_updater>Aa_only_hom_testing,
-                                        <fitness_function_finalizer>additive_finalize_testing,
+                                        <fitness_function_finalizer>return_w_plus1,
                                         0.0)
