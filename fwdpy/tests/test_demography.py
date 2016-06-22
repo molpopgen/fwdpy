5505ef3afa3f62b22d051c862cee660ba836d4f0
Kevin R. Thornton <krthornt@uci.edu>
2016-06-22 12:00:46 -0700

    update unit tests

--- fwdpy/tests/test_demography.py
+++ fwdpy/tests/test_demography.py
@@ -3,19 +3,19 @@ import fwdpy as fp
 import fwdpy.demography as demog
 import numpy as np
 
-class test_make_mpopvec(unittest.TestCase):
+class test_make_MetaPopVec(unittest.TestCase):
     """
     Straightfoward tests to make sure
     that copying from singlepop -> metapop 
     is working.
     """
     def testLength(self):
-        s = fp.popvec(64,100)
-        m = demog.make_mpopvec(s)
+        s = fp.SpopVec(64,100)
+        m = demog.make_MetaPopVec(s)
         self.assertEqual(len(m),len(s))
     def testSizes(self):
-        s = fp.popvec(64,100)
-        m = demog.make_mpopvec(s)
+        s = fp.SpopVec(64,100)
+        m = demog.make_MetaPopVec(s)
         for i in m:
             self.assertEqual(i.popsizes(),[100])
 
@@ -26,12 +26,12 @@ class test_exceptions(unittest.TestCase):
     for details
     """
     def testCopyOutOfRange(self):
-        m = fp.mpopvec(64,[100])
+        m = fp.MetaPopVec(64,[100])
         ##There is only 1 deme, so index of 1 is out of range
         with self.assertRaises(IndexError):
             demog.copy_pop(m,1)
     def testMergeOutOfRange(self):
-        m = fp.mpopvec(64,[100])
+        m = fp.MetaPopVec(64,[100])
         #Make identical copy of
         #first deme in each pop
         demog.copy_pop(m,0)
@@ -41,28 +41,28 @@ class test_exceptions(unittest.TestCase):
             #out of range:
             demog.merge_pops(m,0,2)
     def testMergeDemeOntoItself(self):
-        m=fp.mpopvec(64,[100,100])
+        m=fp.MetaPopVec(64,[100,100])
         with self.assertRaises(RuntimeError):
             ##Cannot merge deme into itself:
             demog.merge_pops(m,1,1)
     def testRemoveOutOfRange(self):
-        m = fp.mpopvec(64,[100,100])
+        m = fp.MetaPopVec(64,[100,100])
         #remove the second deme
         demog.remove_pop(m,1)
         #try again = exception
         with self.assertRaises(IndexError):
             demog.remove_pop(m,1)
     def testSwapOutOfRange(self):
-        m = fp.mpopvec(64,[100,100])
+        m = fp.MetaPopVec(64,[100,100])
         with self.assertRaises(IndexError):
             demog.swap_pops(m,0,2)
     def testSplitOutOfRange(self):
-        m = fp.mpopvec(64,[100])
+        m = fp.MetaPopVec(64,[100])
         rng=fp.GSLrng(101)
         with self.assertRaises(IndexError):
             demog.split_pops(rng,m,2,50)
     def testAdmixOutOfRange(self):
-        m = fp.mpopvec(64,[100,100])
+        m = fp.MetaPopVec(64,[100,100])
         rng=fp.GSLrng(101)
         with self.assertRaises(IndexError):
             demog.admix_pops(rng,m,0,2,0.1,100)
@@ -70,7 +70,7 @@ class test_exceptions(unittest.TestCase):
         """
         Test requirement that admix prop is 0 <= p <= 1
         """
-        m = fp.mpopvec(64,[100,100])
+        m = fp.MetaPopVec(64,[100,100])
         rng=fp.GSLrng(101)
         with self.assertRaises(RuntimeError):
             demog.admix_pops(rng,m,0,1,-0.1,100)
--- fwdpy/tests/test_qtrait.py
+++ fwdpy/tests/test_qtrait.py
@@ -1,33 +1,37 @@
 try:
     import fwdpy.qtrait
+    import fwdpy.fitness
     import unittest
     import array
     #Some basic setup
     rng = fwdpy.GSLrng(101)
     nlist = array.array('I',[1000]*10)
-    
+    f = fwdpy.fitness.SpopGBR()
+    pops=fwdpy.SpopVec(10,1000)
+    n = fwdpy.NothingSampler(len(pops))
+
     class SregionErrors(unittest.TestCase):
         """
         The gene-based region functions cannot allow effect size < 0.
         """
         def testGaussian(self):
             with self.assertRaises(RuntimeError):
-                pops = fwdpy.qtrait.evolve_gbr(rng,1,1000,nlist[0:],0,0.001,0.,[],[fwdpy.GaussianS(0,1,1,0.25)],[],0.025)
+                fwdpy.qtrait.evolve_regions_qtrait_sampler_fitness(rng,pops,n,f,nlist[0:],0,0.001,0.,[],[fwdpy.GaussianS(0,1,1,0.25)],[],1,0.025)
         def testConstant(self):
             with self.assertRaises(RuntimeError):
-                pops = fwdpy.qtrait.evolve_gbr(rng,1,1000,nlist[0:],0,0.001,0.,[],[fwdpy.ConstantS(0,1,1,-0.25)],[],0.025)
+                fwdpy.qtrait.evolve_regions_qtrait_sampler_fitness(rng,pops,n,f,nlist[0:],0,0.001,0.,[],[fwdpy.ConstantS(0,1,1,-0.25)],[],1,0.025)
         def testExp(self):
             with self.assertRaises(RuntimeError):
-                pops = fwdpy.qtrait.evolve_gbr(rng,1,1000,nlist[0:],0,0.001,0.,[],[fwdpy.ExpS(0,1,1,-0.25)],[],0.025)
+                fwdpy.qtrait.evolve_regions_qtrait_sampler_fitness(rng,pops,n,f,nlist[0:],0,0.001,0.,[],[fwdpy.ExpS(0,1,1,-0.25)],[],1,0.025)
         def testGamma(self):
             with self.assertRaises(RuntimeError):
-                pops = fwdpy.qtrait.evolve_gbr(rng,1,1000,nlist[0:],0,0.001,0.,[],[fwdpy.GammaS(0,1,1,-0.25,2)],[],0.025)
+                fwdpy.qtrait.evolve_regions_qtrait_sampler_fitness(rng,pops,n,f,nlist[0:],0,0.001,0.,[],[fwdpy.GammaS(0,1,1,-0.25,2)],[],1,0.025)
         def testUniformLo(self):
             with self.assertRaises(RuntimeError):
-                pops = fwdpy.qtrait.evolve_gbr(rng,1,1000,nlist[0:],0,0.001,0.,[],[fwdpy.UniformS(0,1,1,-0.25,2)],[],0.025)
+                fwdpy.qtrait.evolve_regions_qtrait_sampler_fitness(rng,pops,n,f,nlist[0:],0,0.001,0.,[],[fwdpy.UniformS(0,1,1,-0.25,2)],[],1,0.025)
         def testUniformHi(self):
             with self.assertRaises(RuntimeError):
-                pops = fwdpy.qtrait.evolve_gbr(rng,1,1000,nlist[0:],0,0.001,0.,[],[fwdpy.UniformS(0,1,1,-0.2,-0.1)],[],0.025)
+                fwdpy.qtrait.evolve_regions_qtrait_sampler_fitness(rng,pops,n,f,nlist[0:],0,0.001,0.,[],[fwdpy.UniformS(0,1,1,-0.2,-0.1)],[],1,0.025)
                 
 except ImportError:
     pass
