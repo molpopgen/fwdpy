31afc319773bb1211513da1b00ad14183bc06967
Kevin R. Thornton <krthornt@uci.edu>
2016-06-24 09:38:54 -0700

    Add exponential_size_change function

--- fwdpy/demography/demography.pyx
+++ fwdpy/demography/demography.pyx
@@ -1,4 +1,5 @@
 from fwdpy.fwdpy cimport Spop,MetaPop,SpopVec,MetaPopVec,GSLrng
+import math
 
 ##cdef function act directly on C++ types
 
@@ -217,3 +218,23 @@ def swap_pops(MetaPopVec mpops, size_t deme_i, size_t deme_j):
     """
     for i in mpops:
         swap_demes_details(i,deme_i,deme_j)
+
+def exponential_size_change(Nstart,Nstop,time):
+    """
+    Generate a list of population sizes according to exponential size_change model
+
+    :param Nstart: population size at onset of size change
+    :param Nstop: Population size to reach at end of size change
+    :param time: Time (in generations) to get from Nstart to Nstop
+
+    :return: A list of integers representing population size over time.
+    """
+    if time < 1:
+        raise RuntimeError("time must be >= 1")
+    if Nstart < 1 or Nstop < 1:
+        raise RuntimeError("Nstart and Nstop must both be >= 1")
+    G = math.exp((math.log(Nstop) - math.log(Nstart))/time)
+    rv=[]
+    for i in range(time):
+        rv.append(round(Nstart*pow(G,i+1)))
+    return rv
