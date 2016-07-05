270da066fb03903e5f2744bd389760cf81e00573
Kevin R. Thornton <krthornt@uci.edu>
2016-07-05 13:43:15 -0700

    comment the example

--- extension_tests/run_custom_fitness.py
+++ extension_tests/run_custom_fitness.py
@@ -1,7 +1,9 @@
+#The next three lines process and compile our custom fitness module:
 import pyximport
 pyximport.install()
-import fwdpy as fp
 import test_fwdpy_extensions.test_custom_fitness as tfp
+#import fwdpy and numpy as usual
+import fwdpy as fp
 import numpy as np
 
 rng = fp.GSLrng(101)
@@ -13,6 +15,7 @@ n=np.array([1000]*1000,dtype=np.uint32)
 nr=[fp.Region(0,1,1)]
 sr=[fp.ExpS(0,1,1,0.1)]
 
+#Now, let's do some evolution with our 'custom' fitness functions:
 fitness = tfp.AdditiveFitnessTesting()
 fp.evolve_regions_sampler_fitness(rng,p,s,fitness,n,0.001,0.001,0.001,nr,sr,nr,1)
 
