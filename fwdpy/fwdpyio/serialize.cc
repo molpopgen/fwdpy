4b244f718c0bb9874b53e8585effaa5319c2ce5b
Kevin R. Thornton <krthornt@uci.edu>
2016-07-28 16:34:15 -0700

    some local templates to reduce code duplication

--- fwdpy/fwdpyio/serialize.cc
+++ fwdpy/fwdpyio/serialize.cc
@@ -2,84 +2,59 @@
 
 using namespace std;
 
-namespace fwdpy
-{
-namespace serialize
-{
-string serialize_singlepop(const fwdpy::singlepop_t * pop)
-{
+namespace fwdpy {
+namespace serialize {
+
+template<typename poptype>
+string serialize_details(const poptype * pop) {
     KTfwd::serialize rv;
     rv.buffer.write(reinterpret_cast<const char *>(&(pop->generation)),sizeof(unsigned));
     rv(*pop,KTfwd::mutation_writer(),fwdpy::diploid_writer());
     return rv.buffer.str();
 }
 
-vector<shared_ptr<singlepop_t> > deserialize_singlepop(const vector<string> & strings)
-{
-    vector<shared_ptr<singlepop_t> > rv;
-    for(unsigned i=0; i<strings.size(); ++i)
-    {
-        KTfwd::serialize st;
-        st.buffer.str(strings[i]);
-        st.buffer.seekg(0);
-        singlepop_t pop(0);
-        st.buffer.read(reinterpret_cast<char*>(&pop.generation),sizeof(unsigned));
-        KTfwd::deserialize d;
-        d(pop,st,KTfwd::mutation_reader<KTfwd::popgenmut>(),fwdpy::diploid_reader());
-        rv.emplace_back( shared_ptr<singlepop_t>(new singlepop_t(move(pop))) );
+string serialize_singlepop(const fwdpy::singlepop_t * pop) {
+    return serialize_details(pop);
+}
+
+string serialize_metapop(const fwdpy::metapop_t * pop) {
+    return serialize_details(pop);
+}
+
+string serialize_multilocus(const fwdpy::multilocus_t * pop) {
+    return serialize_details(pop);
+}
+
+template<typename poptype>
+struct deserialize_details {
+    template<typename ...constructor_data>
+    vector<shared_ptr<poptype> > operator()(const vector<string> & strings,
+                                            constructor_data... cdata) {
+        vector<shared_ptr<poptype> > rv;
+        for(unsigned i=0; i<strings.size(); ++i) {
+            KTfwd::serialize st;
+            st.buffer.str(strings[i]);
+            st.buffer.seekg(0);
+            poptype pop(cdata...);
+            st.buffer.read(reinterpret_cast<char*>(&pop.generation),sizeof(unsigned));
+            KTfwd::deserialize d;
+            d(pop,st,KTfwd::mutation_reader<KTfwd::popgenmut>(),fwdpy::diploid_reader());
+            rv.emplace_back( shared_ptr<poptype>(new poptype(move(pop))) );
+        }
+        return rv;
     }
-    return rv;
+};
+
+vector<shared_ptr<singlepop_t> > deserialize_singlepop(const vector<string> & strings) {
+    return deserialize_details<singlepop_t>()(strings,0u);
 }
 
-string serialize_metapop(const fwdpy::metapop_t * pop)
-{
-    KTfwd::serialize rv;
-    rv.buffer.write(reinterpret_cast<const char *>(&(pop->generation)),sizeof(unsigned));
-    rv(*pop,KTfwd::mutation_writer(),fwdpy::diploid_writer());
-    return rv.buffer.str();
+vector<shared_ptr<metapop_t> > deserialize_metapop(const vector<string> & strings) {
+    return deserialize_details<metapop_t>()(strings,0u);
 }
 
-vector<shared_ptr<metapop_t> > deserialize_metapop(const vector<string> & strings)
-{
-    vector<shared_ptr<metapop_t> > rv;
-    for(unsigned i=0; i<strings.size(); ++i)
-    {
-        KTfwd::serialize st;
-        st.buffer.str(strings[i]);
-        st.buffer.seekg(0);
-        metapop_t pop({0});
-        st.buffer.read(reinterpret_cast<char*>(&pop.generation),sizeof(unsigned));
-        KTfwd::deserialize d;
-        d(pop,st,KTfwd::mutation_reader<KTfwd::popgenmut>(),fwdpy::diploid_reader());
-        rv.emplace_back( shared_ptr<metapop_t>(new metapop_t(move(pop))) );
-    }
-    return rv;
-}
-
-string serialize_multilocus(const fwdpy::multilocus_t * pop)
-{
-    KTfwd::serialize rv;
-    rv.buffer.write(reinterpret_cast<const char*>(&(pop->generation)),sizeof(unsigned));
-    rv(*pop,KTfwd::mutation_writer(),fwdpy::diploid_writer());
-    return rv.buffer.str();
-}
-
-vector<shared_ptr<multilocus_t> > deserialize_multilocus(const vector<string> & strings)
-{
-    vector<shared_ptr<multilocus_t> > rv;
-    for(unsigned i=0; i<strings.size(); ++i)
-    {
-        KTfwd::serialize st;
-        st.buffer.str(strings[i]);
-        st.buffer.seekg(0);
-        multilocus_t pop({0,0});
-        st.buffer.read(reinterpret_cast<char*>(&pop.generation),sizeof(unsigned));
-        KTfwd::deserialize d;
-        d(pop,st,KTfwd::mutation_reader<KTfwd::popgenmut>(),fwdpy::diploid_reader());
-        rv.emplace_back( shared_ptr<multilocus_t>(new multilocus_t(move(pop))) );
-    }
-    return rv;
+vector<shared_ptr<multilocus_t> > deserialize_multilocus(const vector<string> & strings) {
+    return deserialize_details<multilocus_t>()(strings,0u,0u);
 }
 }
 }
-
