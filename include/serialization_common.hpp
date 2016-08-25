07b6bc56db1a00ef018662ab61fde51b7667c5c2
Kevin R. Thornton <krthornt@uci.edu>
2016-08-25 14:19:39 -0700

    astyle

--- include/serialization_common.hpp
+++ include/serialization_common.hpp
@@ -8,13 +8,13 @@ namespace fwdpy {
 namespace serialization {
 
 template<typename poptype,
-	typename mwriter_t,
-	typename dipwriter_t>
+         typename mwriter_t,
+         typename dipwriter_t>
 std::string serialize_details(const poptype * pop,
-		const mwriter_t & mwriter,
-		const dipwriter_t & dipwriter) {
+                              const mwriter_t & mwriter,
+                              const dipwriter_t & dipwriter) {
     KTfwd::serialize rv;
-	std::ostringstream buffer;
+    std::ostringstream buffer;
     buffer.write(reinterpret_cast<const char *>((pop->generation)),sizeof(unsigned));
     rv(buffer,*pop,mwriter,dipwriter);
     return buffer.str();
