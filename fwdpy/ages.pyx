124c16b633dc1a80e697af631a1b5106c67eff81
Kevin R. Thornton <krthornt@uci.edu>
2016-09-07 16:02:23 -0700

    allow processing of trajectories in parallel.  User's choice.

--- fwdpy/ages.pyx
+++ fwdpy/ages.pyx
@@ -35,17 +35,39 @@ def merge_trajectories(list trajectories1, list trajectories2):
         rv.append(t)
     return rv
 
-def tidy_trajectories(freqTrajectories trajectories, unsigned min_sojourn = 0, double min_freq = 0.0,
+def tidy_trajectories_details(freqTrajectories trajectories,unsigned min_sojourn, double min_freq,
+        unsigned remove_arose_after,unsigned remove_gone_before):
+    return tidy_trajectory_info(trajectories.thisptr,min_sojourn,min_freq,remove_gone_before,remove_arose_after)
+
+def tidy_trajectories_details(list trajectories,unsigned min_sojourn, double min_freq,
+        unsigned remove_arose_after,unsigned remove_gone_before):
+    cdef vector[freqTraj] vft
+    cdef size_t i
+    for i in range(len(trajectories)):
+        vft.push_back((<freqTrajectories>trajectories[i]).thisptr)
+    cdef vector[vector[selected_mut_data_tidy]] rv
+    rv.resize(vft.size())
+    for i in prange(vft.size(),schedule='dynamic',nogil=True):
+        rv[i]=tidy_trajectory_info(vft[i],min_sojourn,min_freq,remove_gone_before,remove_arose_after)
+    return rv
+
+def tidy_trajectories(object trajectories, unsigned min_sojourn = 0, double min_freq = 0.0,
         remove_arose_after = None,remove_gone_before = None):
     """
     Take a set of allele frequency trajectories and 'tidy' them for easier coercion into
     a pandas.DataFrame.
 
-    :param trajectories: A container of mutation frequency trajectories from a simulation.
+    :param trajectories: A :class:`fwdpy.fwdpy.freqTrajectories` or a list of such objects.
     :param min_sojourn: Exclude mutations that segregate for fewer generations than this value.
     :param min_freq: Exclude mutations that never reach a frequency :math:`\\geq` this value.
+    :param remove_arose_after: (None) Do not include mutations whose origin times are > this value.
+    :param remove_gone_before: (None) Do not include mutations whose last recorded generation are <= this value.
 
     .. note:: The sojourn time filter is not applied to fixations.  I'm assuming you are always interested in those.
+
+    .. note:: Passing in a list results in parallel processing of the input.
+
+    .. note:: Passing in a list can result in extreme RAM consumption, and you may wish to use a for loop instead.
     """
     cdef numeric_limits[unsigned] ul
     cdef unsigned raf = ul.max()
@@ -54,4 +76,5 @@ def tidy_trajectories(freqTrajectories trajectories, unsigned min_sojourn = 0, d
         raf=remove_arose_after
     if remove_gone_before is not None:
         rgb=remove_gone_before
-    return tidy_trajectory_info(trajectories.thisptr,min_sojourn,min_freq,rgb,raf)
+    return tidy_trajectories_details(trajectories,min_sojourn,min_freq,raf,rgb)
+
