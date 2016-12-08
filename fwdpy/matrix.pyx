3598c1ccf7c73ca3498779f77f47d0b81653462d
molpopgen <krthornt@uci.edu>
2016-12-08 13:39:21 -0800

    skeleton for new genotype matrix function

--- fwdpy/matrix.pyx
+++ fwdpy/matrix.pyx
@@ -55,3 +55,13 @@ def hap_matrix(pop,list individuals,include_neutral=True,include_selected=True,r
 
     if isinstance(pop,Spop):
         return haplotype_matrix[singlepop_t](deref((<Spop>pop).pop.get()),individuals,keys.first,keys.second,<size_t>deme_)
+
+def geno_matrix(pop,list individuals,include_neutral=True,include_selected=True,remove_fixed=False,min_daf=None,deme=None,keys=None):
+    deme_=deme
+    if deme is None:
+        deme_=0
+    if keys is None:
+        keys_ = get_mutation_keys(pop,individuals,include_neutral,include_selected,remove_fixed,min_daf,deme)
+
+    if isinstance(pop,Spop):
+        return genotype_matrix[singlepop_t](deref((<Spop>pop).pop.get()),individuals,keys.first,keys.second,<size_t>deme_)
