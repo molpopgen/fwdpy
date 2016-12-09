aedfbe6053dd28b503711efbd0064aa0ad2a1ef6
molpopgen <krthornt@uci.edu>
2016-12-09 11:46:17 -0800

    change minimum fwdpp version

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ try:
     version = out.decode('utf-8').rstrip()
     print ("fwdpp version",version," found.")
     FWDPP_V=version
-    if version < '0.5.3':
-        sys.exit("fwdpp >= ,'0.5.3' required, but ",version, "found.")
+    if version < '0.5.4':
+        sys.exit("fwdpp >= ,'0.5.4' required, but ",version, "found.")
 except:
     sys.exit("fwdppConfig not found.  Please install fwdpp (http://github.com/molpopgen/fwdpp)")
 
