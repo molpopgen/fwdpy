2d4c52d2ea88e5bd1f33a902860e7fb27129cc89
molpopgen <krthornt@uci.edu>
2016-12-12 19:31:08 -0800

    longer test so more to view

--- fwdpy/tests/test_views.py
+++ fwdpy/tests/test_views.py
@@ -8,10 +8,10 @@ sregions = [fwdpy.ExpS(1,2,1,-0.1),fwdpy.ExpS(1,2,0.01,0.001)]
 rregions = [fwdpy.Region(0,3,1)]
 rng = fwdpy.GSLrng(100)
 N=1000
-NGENS=100
+NGENS=10000
 popsizes = np.array([N],dtype=np.uint32)
 popsizes=np.tile(popsizes,NGENS)
-pops = fwdpy.evolve_regions(rng,1,N,popsizes[0:],0.001,0.0001,0.001,nregions,sregions,rregions)
+pops = fwdpy.evolve_regions(rng,1,N,popsizes[0:],0.01,0.01,0.001,nregions,sregions,rregions)
 
 #The sum of the gamete counts must be 2*(deme size):
 #mpops = fwdpy.evolve_regions_split(rng,pops,popsizes[0:],popsizes[0:],0.001,0.0001,0.001,nregions,sregions,rregions,[0]*2)
