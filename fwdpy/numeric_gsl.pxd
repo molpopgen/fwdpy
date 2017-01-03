148bf9de9d9dbe217a635b0d13c7fee059c92a57
molpopgen <krthornt@uci.edu>
2017-01-03 15:41:21 -0800

    buffered version of sum of squares

--- fwdpy/numeric_gsl.pxd
+++ fwdpy/numeric_gsl.pxd
@@ -5,6 +5,9 @@ from libcpp.utility cimport pair
 from libcpp.vector cimport vector 
 from cython_gsl cimport gsl_matrix,gsl_vector
 
+cdef cppclass QRdecompBuffers:
+    vector[double] Qbuff,Rbuff,TAUbuff,SUMSbuff
+
 #Regress v onto m via QR decomposition.
 #Return value is the total sum of squares and then
 #the vector of squared sums for each column in m.
@@ -14,3 +17,7 @@ from cython_gsl cimport gsl_matrix,gsl_vector
 #so if you need it again later, copy it before calling this.
 cdef pair[double,vector[double]] sum_of_squares(const gsl_vector * v,
                                                 gsl_matrix * m) nogil
+
+cdef pair[double,vector[double]] sum_of_squares_buff(const gsl_vector * v,
+                                                gsl_matrix * m,
+                                                QRdecompBuffers & b) nogil
--- fwdpy/numeric_gsl.pyx
+++ fwdpy/numeric_gsl.pyx
@@ -75,3 +75,64 @@ cdef pair[double,vector[double]] sum_of_squares(const gsl_vector * v,
     gsl_set_error_handler(error_handler)
     return rv
 
+cdef pair[double,vector[double]] sum_of_squares_buff(const gsl_vector * v,
+                                                gsl_matrix * m,
+                                                QRdecompBuffers & b) nogil:
+    #Turn of GSL error handling
+    cdef gsl_error_handler_t * error_handler = gsl_set_error_handler_off()
+
+    cdef numeric_limits[double] nld
+    cdef pair[double,vector[double]] rv
+    #Set total sum of squars to not-a-number
+    #in case we return due to an error
+    rv.first=nld.quiet_NaN()
+    if v.size != m.size1:
+        #Bad stuff, so return nonsensical value
+        gsl_set_error_handler(error_handler)
+        return rv
+
+    #cdef gsl_vector_ptr_t TAU,SUMS
+    #cdef gsl_matrix_ptr_t Q,R
+    if b.TAUbuff.size() < min(m.size1,m.size2):
+        b.TAUbuff.resize(min(m.size1,m.size2))
+    if b.SUMSbuff.size() < m.size1:
+        b.SUMSbuff.resize(m.size1)
+    if b.Qbuff.size() < m.size1*m.size1:
+        b.Qbuff.resize(m.size1*m.size1)
+    if b.Rbuff.size() < m.size1*m.size2:
+        b.Rbuff.resize(m.size1*m.size2)
+
+    cdef gsl_vector_view TAU = gsl_vector_view_array(b.TAUbuff.data(),min(m.size1,m.size2))
+    cdef gsl_vector_view SUMS = gsl_vector_view_array(b.SUMSbuff.data(),m.size1)
+    cdef gsl_matrix_view Q = gsl_matrix_view_array(b.Qbuff.data(),m.size1,m.size1)
+    cdef gsl_matrix_view R = gsl_matrix_view_array(b.Rbuff.data(),m.size1,m.size2)
+    cdef int gsl_rv
+    gsl_rv = gsl_linalg_QR_decomp(m,&TAU.vector)
+    if gsl_rv != 0: #check for errors
+        gsl_set_error_handler(error_handler)
+        return rv
+    gsl_rv = gsl_linalg_QR_unpack(m,&TAU.vector,&Q.matrix,&R.matrix)
+    if gsl_rv != 0:
+        gsl_set_error_handler(error_handler)
+        return rv
+    gsl_rv = gsl_blas_dgemv(CblasTrans,1.0,&Q.matrix,v,0.0,&SUMS.vector)
+    if gsl_rv != 0:
+        gsl_set_error_handler(error_handler)
+        return rv
+
+    cdef size_t i
+    for i in range(0,m.size2): 
+        rv.second.push_back(gsl_pow_2(gsl_vector_get(&SUMS.vector,i+1)))
+   
+    cdef size_t DF = m.size2-1
+    RSS=0.0
+    for i in range(DF+1,SUMS.vector.size):
+        RSS += gsl_pow_2(gsl_vector_get(&SUMS.vector,i))
+
+    cdef double sqi=0.
+    rv.first = RSS
+    for sqi in rv.second: rv.first += sqi
+
+    gsl_set_error_handler(error_handler)
+    return rv
+
