51accea9b784e970a30d1aa7660b5f8d8764f248
molpopgen <krthornt@uci.edu>
2017-02-09 11:53:19 -0800

    synchronised version now working.  lots of code cleanup, too.

--- fwdpy/fwdpy/traj2sql.cc
+++ fwdpy/fwdpy/traj2sql.cc
@@ -7,7 +7,6 @@
 #include <vector>
 #include <stdexcept>
 #include <cstdio>
-#include <iostream>
 #include <sqlite3.h>
 #include "sampler_selected_mut_tracker.hpp"
 
@@ -54,25 +53,9 @@ namespace
         return sqlite3_exec(db, sql.c_str(), NULL, NULL, &error_message);
     }
 
-    // struct user_data_wrapper
-    //{
-    //    sqlite3 *recipient;
-    //    sqlite3_stmt *recipient_stmt;
-    //    explicit user_data_wrapper(sqlite3 *db_, sqlite3_stmt *stmt_)
-    //        : recipient(db_), recipient_stmt(stmt_)
-    //    {
-    //    }
-    //};
-
     static int
     sql_callback(void *data, int argc, char **argv, char **azColName)
     {
-        // std::cout << "about 2 copy\n";
-        // for(int i=0;i<argc;++i)
-        //{
-        //	cout << argv[i] << ' ';
-        //}
-        // cout << '\n';
         auto dp = reinterpret_cast<sqlite3_stmt *>(data);
         int idx = 1;
         sqlite3_bind_int(dp, idx, atoi(argv[idx - 1])); // generation
@@ -87,37 +70,63 @@ namespace
         int rc = sqlite3_step(dp);
         if (rc != SQLITE_DONE)
             {
-                std::cout << "oh crap\n";
+                throw runtime_error("sql_callback failed: "
+                                    + to_string(__LINE__));
             }
         sqlite3_reset(dp);
         return 0;
     };
 
-    class trajSQL
+    static int
+    sql_callback_onedb(void *data, int argc, char **argv, char **azColName)
     {
-      private:
+        auto dp = reinterpret_cast<sqlite3_stmt *>(data);
+        int idx = 1;
+        sqlite3_bind_int(dp, idx, atoi(argv[idx - 1])); // rep
+        idx++;
+        sqlite3_bind_int(dp, idx, atoi(argv[idx - 1])); // generation
+        idx++;
+        sqlite3_bind_int(dp, idx, atoi(argv[idx - 1]));
+        idx++;
+        sqlite3_bind_double(dp, idx, strtod(argv[idx - 1], NULL));
+        idx++;
+        sqlite3_bind_double(dp, idx, strtod(argv[idx - 1], NULL));
+        idx++;
+        sqlite3_bind_double(dp, idx, strtod(argv[idx - 1], NULL)); // freq
+        int rc = sqlite3_step(dp);
+        if (rc != SQLITE_DONE)
+            {
+                throw runtime_error("sql_callback_onedb failed: "
+                                    + to_string(__LINE__));
+            }
+        sqlite3_reset(dp);
+        return 0;
+    };
+
+    struct trajSQL
+    {
         sqlite3 *db, *memdb;
         sqlite3_stmt *stmt, *memdb_stmt;
         char *error_message;
         unsigned threshold;
 
-      public:
         using callback_fxn_t = int (*)(void *, int, char **, char **);
         explicit trajSQL(sqlite3 *db_, const string &dbname_,
                          const unsigned threshold_, const bool append_);
         virtual ~trajSQL() noexcept;
         void handle_return_values(int rc, int line_num);
-        virtual void create_table(sqlite3 *db_);
-        virtual void create_index(sqlite3 *db_);
-        virtual void prepare_statements();
-        virtual void copy_from_mem_db(callback_fxn_t);
-        unsigned
+        virtual void create_table(sqlite3 *db_); // needs overloading -- done
+        virtual void create_index(sqlite3 *db_); // needs overloading -- done
+        virtual void prepare_statements();       // needs overloading -- Done
+        virtual void copy_from_mem_db(callback_fxn_t c);
+        virtual unsigned
         apply_prepared_statement(sqlite3 *db_, sqlite3_stmt *stmt_,
                                  const unsigned origin, const unsigned pos,
                                  const unsigned esize,
                                  const vector<pair<unsigned, double>> &freqs);
-        virtual void call_operator_details(
-            const fwdpy::selected_mut_tracker::final_t &data);
+        virtual void
+        call_operator_details(const fwdpy::selected_mut_tracker::final_t
+                                  &data); // needs overloading -- Done
         void operator()(const fwdpy::selected_mut_tracker::final_t &data);
     };
 
@@ -126,7 +135,6 @@ namespace
         : db(nullptr), memdb(nullptr), stmt(nullptr), memdb_stmt(nullptr),
           error_message(nullptr), threshold(threshold_)
     {
-        // cout << "in constructor" << endl;
         if (!append_)
             {
                 remove(dbname_.c_str());
@@ -143,15 +151,7 @@ namespace
                 create_index(db);
                 rc = apply_sql_pragma(db, error_message);
                 handle_return_values(rc, __LINE__);
-                // cout << "opened db " << dbname_ << '\n';
             }
-
-        int rc = sqlite3_open(":memory:", &memdb);
-        handle_return_values(rc, __LINE__);
-        create_table(memdb);
-        // cout << "opened :memory:" << endl;
-        rc = apply_sql_pragma(memdb, error_message);
-        handle_return_values(rc, __LINE__);
     }
 
     trajSQL::~trajSQL() noexcept
@@ -181,10 +181,6 @@ namespace
     void
     trajSQL::handle_return_values(int rc, int line_num)
     {
-        // cout << "handling a return value : " << this_thread::get_id() << ' '
-        //     << (rc == SQLITE_OK) << ' ' << line_num << ' '
-        //     << (error_message == NULL) << ' ' << (error_message == nullptr)
-        //     << '\n';
         if (rc != SQLITE_OK)
             {
                 string message(error_message);
@@ -246,10 +242,6 @@ namespace
         for (auto &&fi : freqs)
             {
                 int idx = 1;
-                // cout << "insertions: " << this_thread::get_id() << ' '
-                //     << fi.first << ' ' << origin << ' ' << pos << ' ' <<
-                //     esize
-                //     << ' ' << fi.second << endl;
                 sqlite3_bind_int(stmt_, idx++,
                                  static_cast<int>(fi.first)); // generation
                 sqlite3_bind_int(stmt_, idx++, static_cast<int>(origin));
@@ -270,14 +262,14 @@ namespace
         return static_cast<unsigned>(freqs.size());
     }
 
-    void trajSQL::copy_from_mem_db(callback_fxn_t)
+    void
+    trajSQL::copy_from_mem_db(callback_fxn_t c)
     {
         string sql = "select * from freqs";
         int rc = sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL,
                               &error_message);
         handle_return_values(rc, __LINE__);
-        rc = sqlite3_exec(memdb, sql.c_str(), sql_callback, (void *)stmt,
-                          &error_message);
+        rc = sqlite3_exec(memdb, sql.c_str(), c, (void *)stmt, &error_message);
         handle_return_values(rc, __LINE__);
         rc = sqlite3_exec(db, "COMMIT TRANSACTION;", NULL, NULL,
                           &error_message);
@@ -291,17 +283,18 @@ namespace
         const fwdpy::selected_mut_tracker::final_t &data)
     {
         // unsigned nrecords_passed = 0;
+
+        sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL, &error_message);
         for (auto &&outer : data)
             {
                 for (auto &&inner : outer.second)
                     {
-						sqlite3_exec(db,"BEGIN TRANSACTION;",NULL,NULL,&error_message);
                         apply_prepared_statement(
                             db, stmt, outer.first, inner.first.first,
                             inner.first.second, inner.second);
-						sqlite3_exec(db,"COMMIT TRANSACTION;",NULL,NULL,&error_message);
                     }
             }
+        sqlite3_exec(db, "COMMIT TRANSACTION;", NULL, NULL, &error_message);
     }
 
     void
@@ -309,7 +302,169 @@ namespace
     {
         call_operator_details(data);
     }
-    //
+
+    class trajSQLonedb : public trajSQL
+    {
+      private:
+        sqlite3 *init_db_mutex(const string &dbname, const bool append);
+
+      public:
+        const unsigned label;
+        using base = trajSQL;
+        explicit trajSQLonedb(const string &dbname_, const unsigned threshold_,
+                              const unsigned label_, const bool append_);
+        void prepare_statements() final;
+        unsigned apply_prepared_statement(
+            sqlite3 *db_, sqlite3_stmt *stmt_, const unsigned origin,
+            const unsigned pos, const unsigned esize,
+            const vector<pair<unsigned, double>> &freqs) final;
+        void call_operator_details(
+            const fwdpy::selected_mut_tracker::final_t &data) final;
+        void create_table(sqlite3 *db) final;
+        void create_index(sqlite3 *db) final;
+    };
+
+    trajSQLonedb::trajSQLonedb(const string &dbname_,
+                               const unsigned threshold_,
+                               const unsigned label_, const bool append_)
+        : base(init_db_mutex(dbname_, append_), dbname_, threshold_, true),
+          label(label_)
+    {
+        lock_guard<mutex> lock(dblock);
+        create_table(db);
+        create_index(db);
+        int rc = apply_sql_pragma(db, error_message);
+        handle_return_values(rc, __LINE__);
+        rc = sqlite3_open(":memory:", &memdb);
+        handle_return_values(rc, __LINE__);
+        create_table(memdb);
+        rc = apply_sql_pragma(memdb, error_message);
+        handle_return_values(rc, __LINE__);
+    }
+
+    sqlite3 *
+    trajSQLonedb::init_db_mutex(const string &dbname, const bool append)
+    {
+        if (!append)
+            {
+                remove(dbname.c_str());
+            }
+        sqlite3 *rv;
+        lock_guard<mutex> lock(dblock);
+        int rc = sqlite3_open(dbname.c_str(), &rv);
+        if (rc != SQLITE_OK)
+            {
+                throw std::runtime_error("Could not open " + dbname);
+            }
+        return rv;
+    }
+
+    void
+    trajSQLonedb::create_table(sqlite3 *db_)
+    {
+        string sql("CREATE TABLE IF NOT EXISTS freqs(");
+        sql += "rep int not null, generation int NOT NULL, origin int NOT "
+               "NULL, pos real NOT ";
+        sql += "NULL, esize real NOT NULL, freq real NOT NULL);";
+        int rc = execute_sql_statement(db_, sql, this->error_message);
+        handle_return_values(rc, __LINE__);
+    }
+
+    void
+    trajSQLonedb::create_index(sqlite3 *db_)
+    {
+        string sql
+            = "create index if not exists rep_gen on freqs (rep,generation);";
+        int rc = execute_sql_statement(db_, sql, this->error_message);
+        handle_return_values(rc, __LINE__);
+    }
+
+    void
+    trajSQLonedb::prepare_statements()
+    {
+        string prepped_statement
+            = "insert into freqs values (?1,?2,?3,?4,?5,?6);";
+        sqlite3_prepare_v2(db, prepped_statement.c_str(),
+                           prepped_statement.size(), &stmt, NULL);
+        if (stmt == NULL)
+            {
+                throw runtime_error(
+                    "could not prepare statement for on-disk database: "
+                    + to_string(__LINE__));
+            }
+        sqlite3_prepare_v2(memdb, prepped_statement.c_str(),
+                           prepped_statement.size(), &memdb_stmt, NULL);
+        if (memdb_stmt == NULL)
+            {
+                throw runtime_error(
+                    "could not prepare statement for in-memory database: "
+                    + to_string(__LINE__));
+            }
+    }
+    unsigned
+    trajSQLonedb::apply_prepared_statement(
+        sqlite3 *db_, sqlite3_stmt *stmt_, const unsigned origin,
+        const unsigned pos, const unsigned esize,
+        const vector<pair<unsigned, double>> &freqs)
+    {
+        if (stmt_ == NULL)
+            {
+                throw std::runtime_error("NULL statement encountered");
+            }
+        for (auto &&fi : freqs)
+            {
+                int idx = 1;
+                sqlite3_bind_int(
+                    stmt_, idx++,
+                    static_cast<int>(this->label)); // replicate ID
+                sqlite3_bind_int(stmt_, idx++,
+                                 static_cast<int>(fi.first)); // generation
+                sqlite3_bind_int(stmt_, idx++, static_cast<int>(origin));
+                sqlite3_bind_double(stmt_, idx++, pos);
+                sqlite3_bind_double(stmt_, idx++, esize);
+                sqlite3_bind_double(stmt_, idx++, fi.second); // freq
+                int rc = sqlite3_step(stmt_);
+                if (rc != SQLITE_DONE)
+                    {
+                        ostringstream message;
+                        message << "Prepared statement could not be executed: "
+                                << this_thread::get_id() << ' ' << __LINE__
+                                << ' ' << idx << ' ' << rc << '\n';
+                        throw std::runtime_error(message.str().c_str());
+                    }
+                sqlite3_reset(stmt_);
+            }
+        return static_cast<unsigned>(freqs.size());
+    }
+
+    void
+    trajSQLonedb::call_operator_details(
+        const fwdpy::selected_mut_tracker::final_t &data)
+    {
+        unsigned nrecords_passed = 0;
+
+        for (auto &&outer : data)
+            {
+                for (auto &&inner : outer.second)
+                    {
+                        nrecords_passed += apply_prepared_statement(
+                            memdb, memdb_stmt, outer.first, inner.first.first,
+                            inner.first.second, inner.second);
+                        if (nrecords_passed > threshold)
+                            {
+                                lock_guard<mutex> lock(dblock);
+                                copy_from_mem_db(sql_callback_onedb);
+                                nrecords_passed = 0;
+                            }
+                    }
+            }
+        if (nrecords_passed)
+            {
+                lock_guard<mutex> lock(dblock);
+                copy_from_mem_db(sql_callback_onedb);
+            }
+    }
+
     // database columns are:
     // rep (int): only if onedb == true
     // generation (int)
@@ -331,10 +486,11 @@ namespace
                      unsigned threshold, const unsigned label,
                      const bool onedb, const bool append)
     {
-        // trajSQL(sqlite3 *db_, const string &dbname_,
-        //                 const unsigned threshold_, const bool append_);
         if (onedb)
             {
+                trajSQLonedb t(dbname, threshold, label, append);
+                t.prepare_statements();
+                t(data.final());
             }
         else
             {
@@ -343,7 +499,6 @@ namespace
                         ostringstream db;
                         db << dbname << '.' << label << ".db";
                         auto name = db.str();
-                        // cout << name << '\n';
                         trajSQL t(NULL, name, threshold, append);
                         t.prepare_statements();
                         t(data.final());
@@ -355,6 +510,7 @@ namespace
             }
     }
 }
+
 namespace fwdpy
 {
     bool
